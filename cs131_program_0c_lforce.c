80ff23fadd6766760e2e32979fd567f8bcb89e69
Larry <Ltforce12@wells.edu>
2012-08-28 16:18:08 -0400

    I have decided to assume FIRSTNAME and LASTNAME likely need to be replaced since they have the all caps thing that FLAST has. Also, I feel like it would come off as less derpy to replace them and be wrong than to not replace them and be wrong.

--- cs131_program_0c_lforce.c
+++ cs131_program_0c_lforce.c
@@ -2,6 +2,6 @@
 
 int main (int argc, char* argv[])
 {
-    printf( "Hello, world. My name is FIRSTNAME LASTNAME, and I am in CS131.\n") ;
+    printf( "Hello, world. My name is Larry Force, and I am in CS131.\n") ;
     return 0;
 }   // main
