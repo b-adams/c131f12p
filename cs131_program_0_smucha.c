99f49a3d94b6e44dc550a0c9252d78d5ce8c151b
Stacey Lynne Mucha <smucha@eclipse.wells.edu>
2012-08-29 14:42:29 -0400

    Changed Hello there, World to Hello, World. My name is Stacey Mucha, and I am in CS131

--- cs131_program_0_smucha.c
+++ cs131_program_0_smucha.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(int argc, char* argv[])
 {
-  printf("Hello %s, World!\n", "there");
+  printf("Hello, world. My name is Stacey Mucha, and I am in CS131.\n");
   return 0;
 }
