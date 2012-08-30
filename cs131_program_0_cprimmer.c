37ac308a9832cb8711654648d82cade8a3a8d79d
cprimmer@wells.edu <cprimmer@eclipse.wells.edu>
2012-08-29 22:45:02 -0400

    i fixed the im to i'm and introduced myself

--- cs131_program_0_cprimmer.c
+++ cs131_program_0_cprimmer.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(int argc, char* argv[])
 {
-  printf("hello world, my name is Cody Primmer and im in CS131\n");
+  printf("hello world, my name is Cody Primmer and i'm in CS131\n");
   return 0;
 }
