fc55e5b785bbb02fad81ac3e21b95aabbefda779
jessica Corter <jessica corter>
2012-09-07 15:50:15 -0400

    added nextInteger frunction definition

--- cs131_program_1_jcorter.c
+++ cs131_program_1_jcorter.c
@@ -194,7 +194,11 @@ printf("\tThe Ulam number after %d is %d\n",
 return (previous%2)?(previous/2):(3*previous+1);
 }
 
-//TASK 5: PUT nextInteger FUNCTION DEFINITION HERE
+int nextInteger (int previous)
+{
+    printf("\tThe integer after %d is %d\n", previous, previous + 1)
+    return (previous + 1) 
+}
 
 int averageOfThree(int first, int second, int third)
 {
