208d846cf657af468ef78fe7743cb6b30e5f0c33
Colby Schrom <cschrom12@wells.edu>
2012-09-06 21:25:09 -0400

    Completed Task 5.

--- cs131_program_1_cschrom.c
+++ cs131_program_1_cschrom.c
@@ -5,39 +5,6 @@
  * @date 9/6/2012
  */
 
-// INSTRUCTIONS:
-//   cd ~/cs131
-//   touch cs131_program_1_FLAST.c
-//   git add cs131_program_1_FLAST.c
-//   git commit -m "Initial creation of Program 1 source code file"
-//
-//   Open cs131_program_1_FLAST.c for editing (in Komodo or Pico)
-//   Paste the contents of this file into cs131_program_1_FLAST.c
-//   Modify the documentation at the very top to fix the FLAST, YOUR NAME HERE, and THE CURRENT DATE
-//   Save a snapshot with commit message "Starting content added to file"
-//     (Save your changes, 'git add' again, and 'git commit' with the requested message)
-//
-// There are four more tasks.
-//   Each task has a line that starts with "// TASK"
-//   You may do the rest of the tasks in any order
-//   Once you have followed the instructions for a task:
-//     Delete the "// TASK..." comment
-//     Save a snapshot with a message describing the task you completed
-//   Once you have done all the tasks, and your program compiles and runs:
-//     Add a line to the documentation at the top that says:
-//        * @status Working!
-//     Delete this big block of comments
-//     Double check there are no other // comments in your file
-//     Make one more snapshot with a message like "Done final cleanup"
-//     Tag your snapshot FLAST.prog.1
-//     Turn in your work in Moodle
-//
-// FYI, the task lines will ask you to:
-//   Write a documentation block for squareOf
-//   Write a declaration for the nextFibonacci function
-//   Write the function calls that find the averages of (1,2,3), (10, 10, 10), (11, 10, 10), (10, 12, 10), and (10,10,13)
-//   Write a definition for the nextInteger function
-
 #include <stdio.h>
 
 /**
@@ -136,7 +103,7 @@ int main(int argc, char* argv[])
     printf("\n\n");
    
     printf("Averages...\n");
-        int averageOf(1,2,3)
+        int averageOfThree(1,2,3)
         int averageOfThree(10,10,10)
         int averageOfThree(11,10,10)
         int averageOfThree(10,12,10)
@@ -194,7 +161,11 @@ int nextUlam(int previous)
 	return (previous%2)?(previous/2):(3*previous+1);
 }
 
-//TASK 5: PUT nextInteger FUNCTION DEFINITION HERE
+int nextInteger(int previous);
+{
+    printf(The integer after %d is%d\n, previous, previous+1);
+    return previous+1;
+}
 
 int averageOfThree(int first, int second, int third)
 {
