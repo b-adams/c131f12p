43e530cf92f1068245145b38f6022a6f255c7d45
Danielle Crabtree <dcrabtree@eclipse.wells.edu>
2012-09-16 23:17:42 -0400

    Added documentation to program

--- cs131_program_0_dcrabtree.c
+++ cs131_program_0_dcrabtree.c
@@ -1,7 +1,24 @@
+/**
+ *@cs131_program_0_dcrabtree.c
+ *@author Danielle Crabtree
+ *@date September 16, 2012
+ *@brief Program 0: Hello World Program
+ *@status Working!
+ */
+
 #include <stdio.h>
+
+/**
+ *@author Danielle Crabtree
+ *@brief Where the program starts running (Main procedure).
+ *@param void The function will not return a value.
+ *@sideeffect The program is the side effect.
+ *@return Returns 0 if program is successful.
+ */
 
 int main (void)
 {
   printf("Hello, world. My name is Danielle Crabtree, and I am in CS131.\n");
   return 0;
 } //main
+
