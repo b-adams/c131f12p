d5dba1f873444f0bca43c6042c363f38dc30e858
Eric Shane <ewshane@hotmail.com>
2012-09-17 13:43:54 -0400

    Added documentation to program 0d.

--- cs131_program_0_eshane.c
+++ cs131_program_0_eshane.c
@@ -1,8 +1,24 @@
+/**
+  *@file cs131_program_0_eshane.c
+  *@author Eric Shane
+  *@date 9/17/12
+  *@status works
+  *@brief prints: Hello World, Name, and Class
+ */
+
 #include <stdio.h>
+
+/**
+  *@brief Main procedure
+  *@param argc
+  *@param argv
+  *@return nothing
+  *@author Eric Shane
+*/
+
 
 int main(int argc, char* argv[])
 {
-
-  printf("Hello World!\nMy Name is %s\nI am in CS131.\n" , "Eric Shane!");
-  return 0;
+    printf ("Hello World!\nMy Name is %s\nI am in CS131.\n" , "Eric Shane!");
+    return 0;
 }
