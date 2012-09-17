9009e0cc4953219999f58f99d881e86de2fabc44
Melissa Lynn Fortin <mfortin@eclipse.wells.edu>
2012-09-17 13:48:43 -0400

    I added documentation to program 0c and it's now 0d

---
+++ cs131_program_0d_mfortin.c
@@ -0,0 +1,30 @@
+/**
+* @file cs131_program_0d_mfortin.c
+* @author MELISSA_FORTIN
+* @date SEPTEMBER_17
+* @brief Program 0d: Adding documentation to program 0c
+*
+* @todo Add documentation to program 0c
+* @done Added documentation to program 0c, and it's now 0d
+*
+* @status DESCRIBE_IF_EVERYTHING_WORKING_OR_WHAT_IS_NOT_YET_RIGHT
+*
+* @assistance TA Joshua Brutscher
+*/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    printf("Hello, World. My name is Melissa Fortin, and I am in CS131\n");
+    return 0;
+}
+
+/**
+* @brief Main procedure.
+*
+* @param argv the command line (typed arguments)
+* @param argc the number of options in the command line.
+* @return zero if successful
+* @author MELISSA_FORTIN
+*/
