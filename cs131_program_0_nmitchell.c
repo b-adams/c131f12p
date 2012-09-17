aaffa151c90e44448d110d4859f29f8943fcf565
Nathan Mitchell <ncmitchell12@wells.edu>
2012-09-17 11:51:27 -0400

    Hello, World program with documentation

--- cs131_program_0_nmitchell.c
+++ cs131_program_0_nmitchell.c
@@ -1,5 +1,18 @@
+/**
+ * @file cs131_program_0_nmitchell.c
+ * @author Nathan Mitchell
+ * @date September 17, 2012
+ * @status working
+ */
 #include <stdio.h>
 
+/**
+ *@brief Main procedure
+ *@param argc the command line
+ *@param argv the number of options in the command line
+ *return zero if succseful
+ *@author Nathan Mitchell
+ */
 int main(int argc, char* argv[])
 {
   printf("Hello, World! %s, and I am in CS131\n","My name is Nathan Mitchell");
