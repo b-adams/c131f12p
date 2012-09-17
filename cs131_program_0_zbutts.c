5548f75567f4fe64c6bd29058276eea82c82af37
Zakarra Butts <zmbutts12@wells.edu>
2012-09-17 11:50:49 -0400

    Adding documentation to program 0

--- cs131_program_0_zbutts.c
+++ cs131_program_0_zbutts.c
@@ -1,4 +1,20 @@
+/**
+ *@file cs131_program_0_zbutts.c
+ *@author Zakarra Butts
+ *@date 9/17/12
+ *@status working
+ */
+
 #include <stdio.h>
+
+/**
+ *@brief Main procedure
+ *
+ *@param argv the command line
+ *@param argc the number of options in the command line
+ *@return zero if successful
+ *@author Zakarra Butts
+ */
 int main(int argc, char* argv[])
 {
   printf("Hello, world. %s\n", "My name is Zakarra Butts, and I am in CS131.");
