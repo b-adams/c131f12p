14f959800859530f0ef8464ae74f962a04627cd5
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-09-17 11:51:33 -0400

    Documentation added before main

--- cs131_program_0_pbadianpessot.c
+++ cs131_program_0_pbadianpessot.c
@@ -9,8 +9,14 @@
 #include <stdio.h>
 
 /**
-* @brief 
+* @brief main procefure
+* @param argv the command line (typed arguments)
+* @param argc the number of options in the command line.
+* @return zero if successful
+* @author Pamela Badian-Pessot
+* @ sideeffect prints "Hello, world. My name is Pamela Badian-Pessot and I am in CS131." 
 */
+
 int main(int argc, char* argv[])
 {
   printf("Hello, world. My name is Pamela Badian-Pessot and I am in CS131.");
