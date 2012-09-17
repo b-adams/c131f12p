dd97a50f0ecd8be58eb6eff83424e3acb7e11245
Stacey Lynne Mucha <smucha@eclipse.wells.edu>
2012-09-17 13:58:45 -0400

    Added documentation

--- cs131_program_0_smucha.c
+++ cs131_program_0_smucha.c
@@ -1,4 +1,23 @@
+/**
+ *@file cs131_program_0_smucha.c
+ *@author Stacey Mucha
+ *@date Sept 17
+ *@status Working!
+*/
+
 #include <stdio.h>
+
+/**
+* @brief Main procedure.
+*
+* @param argv the command line (typed arguments)
+* @param argc the number of options in the command line.
+* @return zero if successful
+* @author Stacey Mucha
+*
+* This program should display "Hello, world. My name is Stacey Mucha, and I am in CS131."
+*/
+
 int main(int argc, char* argv[])
 {
   printf("Hello, world. My name is Stacey Mucha, and I am in CS131.\n");
