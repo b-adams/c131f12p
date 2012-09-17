2ceab8c59d51e164360f44a749cdb1cf1d067184
jake smith <jkesmth@gmail.com>
2012-09-17 11:50:52 -0400

    program 0 ready

--- cs131_program_0_jsmith.c
+++ cs131_program_0_jsmith.c
@@ -1,4 +1,22 @@
+/**
+ * @file cs131_program_0_FLAST.c
+ * @author Jsmith
+ * @date Sept 17 2012
+ * @status Working
+ * @brief Program 0: Says "Hello World! My name is Jake, and I am in cs131."
+ */
+
+
 #include <stdio.h>
+
+/**
+* @brief Main procedure.
+*
+* @param argv the command line (typed arguments)
+* @param argc the number of options in the command line.
+* @return zero if successful
+* @author Jsmith
+*/
 
 int main(int argc, char* argv[])
 {
