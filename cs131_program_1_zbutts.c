6bf54ffbca3921d289c781deb5b126432830f88c
Zakarra Butts <zmbutts12@wells.edu>
2012-09-17 17:07:05 -0400

    Fixed errors...possibly

--- cs131_program_1_zbutts.c
+++ cs131_program_1_zbutts.c
@@ -186,9 +186,9 @@ return (previous%2)?(previous/2):(3*previous+1);
 
 int nextInteger (int previous)
 {
-    printf ("\tThe next integer after %d is %d\n")
-            previous;
-return (next integer of the previous integer)
+    printf ("\tThe next integer after %d is %d\n"
+            previous);
+return (integer after the previous integer);
 }
 
 int averageOfThree(int first, int second, int third)
