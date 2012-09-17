dee3976f3470f6cd1c71d13f6e018a0e394b8c77
jessica Corter <jessica corter>
2012-09-17 16:50:14 -0400

    Added documentation

--- cs131_program_0_jcorter.c
+++ cs131_program_0_jcorter.c
@@ -1,4 +1,22 @@
+/**
+ * @file cs131_program_0_jcorter.c
+ * @author Jessica Corter
+ * @date 09/17/2012
+ * @status Working and documentation completed
+*/
+
 #include <stdio.h>
+
+/**
+ * @brief Main procedure.
+
+ * @param argv the command line (typed arguments)
+ * @param argc the number of options in the command line.
+ * @return zero if successful
+ * @author Jessica Corter
+
+ This program displays the string, "Hello World. My name is Jessica Corter, and I am in CS131." on the screen.
+*/
 
 int main(int argc, char* argv[])
 
