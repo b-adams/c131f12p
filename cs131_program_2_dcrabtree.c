b0eefb6b2f450c2f52e85e3ac28506ac372baa0b
Danielle Crabtree <dcrabtree@eclipse.wells.edu>
2012-09-18 14:06:28 -0400

    Defined int main so it returns zero

--- cs131_program_2_dcrabtree.c
+++ cs131_program_2_dcrabtree.c
@@ -4,6 +4,11 @@
 * @date September 18, 2012
 * @brief Program 2: The Trick Program
 * @status In Progress
-*
 */
 
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}// main
