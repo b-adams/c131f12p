0e02b16b202488bc18b1da20acdd2dc0daf467ea
eyonce@wells.edu <eyonce@eclipse.wells.edu>
2012-09-17 23:07:59 -0400

    Program 0 documented

--- cs131_program_0_eyonce.c
+++ cs131_program_0_eyonce.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ *@author Emily Yonce
+ *@date 09172012
+ *@brief Prints message on the screen.
+ *@param argc
+ *@param argv
+ */
+
 int main(int argc, char* argv[])
 {
   printf("Hello , world. My name is Emily Yonce, and I am in CS131.\n");
