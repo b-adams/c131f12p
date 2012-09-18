9ae21445590d4a7dcc22c7887a079c11d1b69bff
cprimmer@wells.edu <cprimmer@eclipse.wells.edu>
2012-09-17 23:03:37 -0400

    documentation completed

--- cs131_program_0_cprimmer.c
+++ cs131_program_0_cprimmer.c
@@ -1,3 +1,9 @@
+/**
+ * @file cs131_program_0_cprimmer.c
+ * @author Cody Primmer thats me
+ * @date September 17 2012
+ * @brief Program_0: Hello World Program
+
 #include <stdio.h>
 int main(int argc, char* argv[])
 {
