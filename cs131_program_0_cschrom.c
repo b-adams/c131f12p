f049e61abf9bd2b28085c06ae00d9099f39ca4ae
Colby Schrom <cschrom12@wells.edu>
2012-09-18 18:01:48 -0400

    *PROF ADAMS* Refolding prog0d file into prog0

--- cs131_program_0_cschrom.c
+++ cs131_program_0_cschrom.c
@@ -1,7 +1,22 @@
+/**
+ *@file cs131_program_0d_cschrom.c
+ *@brief The program prints "Hello World!", My name, and my class
+ *@author Colby Schrom
+ *@date 9/17/12
+ *@status working
+ */
+
 #include <stdio.h>
 
+/**
+ *@brief Main Procedure
+ *@param argc
+ *@param argv
+ *@returns nothing
+ *@author Colby Schrom
+ */
 int main(int argc, char* argv[])
 {
-  printf("Hello %s, World!\n" ,"There");
+  printf("Hello, World!\nMy name is Colby Schrom.\nI am in cs131.\n" );
   return 0;
 }
