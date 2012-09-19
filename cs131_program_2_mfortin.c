49b3135db44d405c7a4387f598fe9d2415715f11
Melissa Fortin <mfortin@wells.edu>
2012-09-19 12:13:36 -0400

    I defined int main

--- cs131_program_2_mfortin.c
+++ cs131_program_2_mfortin.c
@@ -5,9 +5,25 @@
 * @brief Program 2: Programming an arithmetic trick involving powers of two
 *
 * @todo Assignment 2
-* @done Added documentation block
+* @done Added documentation block, defined int main
 *
 * @status In progress
 *
 * @assistance TA Josh Brutscher
 */
+
+
+#include <stdio.h>
+/**
+ @brief The answer that the trick always comes up with
+ 
+ For not-very-mysterious algebraic reasons, this trick will always come up
+ with the same answer. Rather than have '2' randomly show up in our code as
+ a 'magic number', we give it a name here.
+*/
+
+int main (int argc, char* argv[])
+{
+  const int THE_ANSWER_EVERY_TIME =2;
+return 0;
+}
