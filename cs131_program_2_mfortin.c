3abf346027ca30685616ab6eb3e9769e36f269b9
Melissa Fortin <mfortin@wells.edu>
2012-09-19 12:09:17 -0400

    I added documentation to program 2

---
+++ cs131_program_2_mfortin.c
@@ -0,0 +1,13 @@
+/**
+* @file cs131_program_2_mfortin.c
+* @author MELISSA_FORTIN
+* @date SEPTEMBER_19
+* @brief Program 2: Programming an arithmetic trick involving powers of two
+*
+* @todo Assignment 2
+* @done Added documentation block
+*
+* @status In progress
+*
+* @assistance TA Josh Brutscher
+*/
