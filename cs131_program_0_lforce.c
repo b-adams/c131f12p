64bbf2fb65cff9357b7d6b783a22ca6e6f621b8e
Larry <Ltforce12@wells.edu>
2012-09-18 20:59:27 -0400

    *PROF ADAMS* Folding prog0d file into prog0

--- cs131_program_0_lforce.c
+++ cs131_program_0_lforce.c
@@ -1,7 +1,25 @@
+/**
+* @file   cs131_program_0d_lforce.c
+* @author Larry Force
+* @date   September 17, 2012
+* @brief  Displays text when run
+* @status working
+* /
+
+
+
 #include <stdio.h>
 
+
+/**
+* @brief Main procedure.
+* @param argv the command line (typed arguments)
+* @param argc the number of options in the command line.
+* @return zero
+* @author Larry Force
+*/
 int main (int argc, char* argv[])
 {
     printf( "Hello, world. My name is Larry Force, and I am in CS131.\n") ;
     return 0;
-}   // main
+}  
