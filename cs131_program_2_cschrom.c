b761406dfadc12d205a1438c37954148b3b4af4f
Colby Schrom <cschrom12@wells.edu>
2012-09-20 21:25:39 -0400

    Opening documentation complete

---
+++ cs131_program_2_cschrom.c
@@ -0,0 +1,9 @@
+/**
+ *@file cs131_program_2_cschrom.c
+ *@Author Colby Schrom
+ *@date 9/19/12
+ *@status In Progress
+ *@assisstance Larry Force, Eric Shane, Jake Smith, Nate Mitchell
+ *@brief The Trick, (program 2)
+ */
+
