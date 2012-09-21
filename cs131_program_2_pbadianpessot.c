734a938e85dbbe8c620aa1641a4994d522335aeb
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-09-20 22:34:10 -0400

    Initial documentation for Program 2

---
+++ cs131_program_2_pbadianpessot.c
@@ -0,0 +1,13 @@
+/**
+* @file cs131_program_2_pbadianpessot.c
+* @author Pamela Badian-Pessot
+* @date September 20, 2012
+* @status In Progress
+*/
+
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+
+}
