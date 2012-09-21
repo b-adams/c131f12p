870d03ea4dea41bef59419566eb6e4ae83277a4f
jessica Corter <jessica corter>
2012-09-21 12:32:50 -0400

    added documentation and declaration for constant, THE_ANSWER_EVERY_TIME.

--- cs131_program_2_jcorter.c
+++ cs131_program_2_jcorter.c
@@ -6,6 +6,17 @@
  *@status in progress
  */
 
+#include <stdio.h>
+
+/**
+ @brief The answer that the trick always comes up with
+ 
+ For not-very-mysterious algebraic reasons, this trick will always come up
+ with the same answer. Rather than have '2' randomly show up in our code as
+ a 'magic number', we give it a name here.
+ */
+
+ const int THE_ANSWER__EVERY_TIME = 2;
 
 /**
  * @brief Main procedure.
