58007cf64f73b5fce48f0f6cdb82c9ce6db60a42
jessica Corter <jessica corter>
2012-09-21 12:27:19 -0400

    I created my file for program 2 and added file documentation.

---
+++ cs131_program_2_jcorter.c
@@ -0,0 +1,9 @@
+/**
+ *@file cs131_program_2_jcorter.c
+ *@author Jessica Corter
+ *@date September 19, 2012
+ *@brief Program 2: This program uses trickery to guess a users final number after several arithmetic operations.
+ *@status in progress
+ */
+
+
