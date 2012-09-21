e96904152febc9621986a98088d3ef41dee4e386
jessica Corter <jessica corter>
2012-09-21 12:35:50 -0400

    added documentation, declaration and definition for twoToThe function.

--- cs131_program_2_jcorter.c
+++ cs131_program_2_jcorter.c
@@ -19,6 +19,19 @@
  const int THE_ANSWER__EVERY_TIME = 2;
 
 /**
+ @brief Compute a power of 2
+ @param exponent The exponent: how many times to multiply 2 by itself.
+ @sideeffect None
+ @returns 2^n
+ 
+ This works by using the fact that 2^n is twice 2^(n-1), and 2^0 is 1
+ So, if the user gives 0 for n, this just returns n
+ Otherwise, it returns twice the value of twoToThe(n-1)
+ [We'll talk about this 'recursion' trick later in the class. It's very useful.]
+ */
+int twoToThe(int exponent);
+
+/**
  * @brief Main procedure.
 
  * @param argv the command line (typed arguments)
@@ -27,6 +40,7 @@
  * @author Jessica Corter
  
 */
+
 int main(int argc, char* argv[]);
 
 int main(int argc, char* argv[])
@@ -35,4 +49,19 @@ int main(int argc, char* argv[])
 
      return 0;
     
+}
+
+int twoToThe(int exponent)
+{
+    int theResult;
+    
+    printf("-> Calling twoToThe(%d)\n",exponent);
+    
+	if(exponent<0) theResult=1/twoToThe(-exponent);
+	else if(exponent==0) theResult=1;
+	else theResult=twice(twoToThe(exponent-1));
+    
+    printf("<- twoToThe(%d) returns %d\n",exponent, theResult);
+    
+    return theResult;
 }
