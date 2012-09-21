a50323dfbf1352e2a899532af1e3e37ae05c9201
Eric Shane <ewshane@hotmail.com>
2012-09-20 21:29:49 -0400

    added int main

---
+++ cs131_program_2_eshane.c
@@ -0,0 +1,37 @@
+/**
+* @file   cs131_program_2_eshane.c
+* @author Eric Shane
+* @date   09/20/12
+* @brief  Program 2: The Trick
+*
+* @todo write a program taht completes the trick
+* @done To do
+* 
+* @status starting
+* 
+* SHARING_YOUR_THOUGHT_PROCESS_HERE_MAY_HELP
+* WITH_GETTING_PARTIAL_CREDIT_IF_THINGS_ARE
+* NOT_ALL_CORRECT
+* 
+* @assistance CITE_NON_COURSE_WEBPAGES_YOU_USED_FOR_HELP
+* @assistance YOU_MAY_HAVE_MULTIPLE_CITATIONS
+* @assistance Larry Force, Jake Smith, Colby Schrom
+* @note Larry Force, Jake Smith, Colby Schrom
+*/
+
+#include <stdio.h>
+
+/**
+* @brief Main procedure.
+*
+* @param argv the command line (typed arguments)
+* @param argc the number of options in the command line.
+* @return zero if successful
+* @author Eric Shane
+*/
+int main( int argc, char * argv[] )
+{
+   return 0;
+}
+
+
