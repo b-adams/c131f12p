49f76b94fc05f7f645f6b326f214a442ab82e731
Nathan Mitchell <ncmitchell12@wells.edu>
2012-09-20 21:33:49 -0400

    step 2 done

--- cs131_program_2_nmitchell.c
+++ cs131_program_2_nmitchell.c
@@ -4,3 +4,16 @@
  * @date Septemeber 6, 2012
  * @status in progress
  */
+
+#include <stdio.h>
+
+/**
+ @brief Main procedure.
+
+ @param argv the command line (typed arguments)
+ @param argc the number of options in the command line.
+ @return zero if successful
+ @author Nathan Mitchell
+*/
+
+int main(int argc, char* argv[])
