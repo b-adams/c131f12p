aad2709e2500ebc8d4e0c0a383324d76fa9b6db0
Colby Schrom <cschrom12@wells.edu>
2012-09-21 00:28:34 -0400

    Added the constant'THE_ANSWER_EVERY_TIME'

--- cs131_program_2_cschrom.c
+++ cs131_program_2_cschrom.c
@@ -7,3 +7,45 @@
  *@brief The Trick, (program 2)
  */
 
+#include <stdio.h>
+
+/**
+ @brief The answer that the trick always comes up with
+ 
+ For not-very-mysterious algebraic reasons, this trick will always come up
+ with the same answer. Rather than have '2' randomly show up in our code as
+ a 'magic number', we give it a name here.
+ */
+
+const int THE_ANSWER_EVERY_TIME = 2
+
+int main(int argc, char* argv[])
+{
+   return 0;
+}
+
+/**
+ @brief Compute a power of 2
+ @param exponent The exponent: how many times to multiply 2 by itself.
+ @sideeffect None
+ @returns 2^n
+ 
+ This works by using the fact that 2^n is twice 2^(n-1), and 2^0 is 1
+ So, if the user gives 0 for n, this just returns n
+ Otherwise, it returns twice the value of twoToThe(n-1)
+ [We'll talk about this 'recursion' trick later in the class. It's very useful.]
+ */
+int twoToThe(int exponent);
+{
+    int theResult;
+    
+    printf("-> Calling twoToThe(%d)\n",exponent);
+    
+	if(exponent<0) theResult=1/twoToThe(-exponent);
+	else if(exponent==0) theResult=1;
+	else theResult=twice(twoToThe(exponent-1));
+    
+    printf("<- twoToThe(%d) returns %d\n",exponent, theResult);
+    
+    return theResult;
+}
