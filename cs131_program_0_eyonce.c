7aca1b7b8944962f90dcee9b0bdac3bcc81cacaa
eyonce@wells.edu <eyonce@eclipse.wells.edu>
2012-09-27 14:03:24 -0400

    I fixed the compiling issue

--- cs131_program_0_eyonce.c
+++ cs131_program_0_eyonce.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 
 /**
+ *@file cs131_program_0_eyonce.c
  *@author Emily Yonce
  *@date 09172012
  *@brief Prints message on the screen.
- *@param argc
- *@param argv
+ */
+
+/**
+ *@brief Main procedure
+ *@param argv the command line
+ *@param argc number of options in the command line
+ *@return zero if succesful
  */
 
 int main(int argc, char* argv[])
 {
   printf("Hello , world. My name is Emily Yonce, and I am in CS131.\n");
+  return 0;
   
 }
