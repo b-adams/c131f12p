dcb1856e25c1977cc032fa9579ba7400953975b4
Melissa Fortin <mfortin@wells.edu>
2012-09-28 11:54:34 -0400

    I completed task 1

---
+++ cs131_test_1_problem_1_mfortin.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+const float C_TO_F_MULTIPLE = 1.8;
+//Todo 1
+
+void displayCelciusInFahrenheit(int tempInC);
+int getBoundedNumber(int minsize, int maxsize);
+
+int main(int argc, char* argv[])
+{
+    int tempToConvert;
+    //Todo 3
+    //Todo 7
+    //Todo 5
+    //Todo 7
+    return 0;
+}
+
+void displayCelciusInFahrenheit(int tempInC)
+{
+    //Todo 2
+    //Todo 4
+    //Todo 6
+}
+
+int getBoundedNumber(int minsize, int maxsize)
+{
+    int userInput;
+    do {
+        printf("Please enter a number between %d and %d\n", minsize, maxsize);\
+        scanf("%d", &userInput);
+    } while(userInput < minsize || userInput > maxsize);
+    
+    return userInput;
+}
