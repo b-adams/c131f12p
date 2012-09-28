e86df2799cd62a9419d15b0eb48572109ac24f2a
jessica Corter <jessica corter>
2012-09-28 12:31:48 -0400

    fixed syntax errors

--- cs131_test_1_problem_2_jcorter.c
+++ cs131_test_1_problem_2_jcorter.c
@@ -1,27 +1,27 @@
 
-include<stdio.h>
+#include<stdio.h>
 
 
-constant SOME_NUMBER;
+const int SOME_NUMBER;
 
 
-int getTwoAndAddTwo(int a, int b)
+int getTwoAndAddTwo(int a, int b);
 
 
-int main()
+int main(int argc, char* argv [])
 {
-    int tomorrow
-    tomorrow=gettwoandaddtwo(4, SOME_NUMBER);
-    print (Please ask about it on Nov %dth "", tomorrow);
+    int tomorrow;
+    tomorrow=getTwoAndAddTwo(4, SOME_NUMBER);
+    print ("Please ask about it on Nov %dth", tomorrow);
     return today;
 }
 
 int getTwoAndAddTwo(a,b)
 {
-    int 1stvar:
-    printf (Pleasee enter two numbers, in the):
-    printf (" form X IS 4 Y IS 5 THERE IS NO Z";
-    int secondvar;
-    scanf("X IS 5d Y is %d THERE IS NO Z", 1stvar, secondvar);
-    a+b+1stvar+secondvar;
+    int firstVar;
+    printf ("Pleasee enter two numbers, in the");
+    printf ("form X IS 4 Y IS 5 THERE IS NO Z");
+    int secondVar;
+    scanf("X IS 5d Y is %d THERE IS NO Z", &FirstVar, &secondVar);
+    return a+b+firstVar+secondVar;
 }
