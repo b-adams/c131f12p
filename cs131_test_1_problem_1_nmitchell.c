f506a99c148398435d48b73601f1d8836593c582
Nathan Mitchell <ncmitchell12@wells.edu>
2012-09-28 12:22:44 -0400

    finished with program it compiles and runs correctly

--- cs131_test_1_problem_1_nmitchell.c
+++ cs131_test_1_problem_1_nmitchell.c
@@ -1,3 +1,10 @@
+/**
+ *@file cs131_test_1_problem_1_nmitchell.c
+ *@author Nathan Mitchell
+ *@date September 28, 2012
+ *@status working
+ */
+
 #include <stdio.h>
 
 const float C_TO_F_MULTIPLE = 1.8;
@@ -9,18 +16,18 @@ int getBoundedNumber(int minsize, int maxsize);
 int main(int argc, char* argv[])
 {
         int tempToConvert;
-    //todo 3
-    //todo 7
-    //todo 5
-    //todo 7 (again)
+    tempToConvert = -40;
+    displayCelsiusInFahrenheit(tempToConvert);
+    tempToConvert = getBoundedNumber(-40, 50);
+    displayCelsiusInFahrenheit(tempToConvert);
     return 0;
 }
 
 void displayCelsiusInFahrenheit(int tempInC)
 {
-    //todo 2
-    //todo 4
-    //todo 6
+    int tempInF;
+    tempInF = tempInC * C_TO_F_MULTIPLE + C_TO_F_SHIFT;
+    printf("%d celsius would be %d fahrenheit \n", tempInC, tempInF);
 }
 
 int getBoundedNumber(int minsize, int maxsize)
@@ -28,7 +35,7 @@ int getBoundedNumber(int minsize, int maxsize)
     int userInput;
     do {
         printf("Please enter a number between %d and %d \n", minsize, maxsize);
-        scanf("%d", &userInput);
+        scanf("%d \n", &userInput);
     }   while(userInput < minsize || userInput > maxsize);
     
     return userInput;
