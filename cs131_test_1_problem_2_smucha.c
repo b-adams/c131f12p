ab695f779f4e3ddc4a7589586f927f7c13d7a077
Stacey Lynne Mucha <smucha@eclipse.wells.edu>
2012-09-28 12:36:32 -0400

    Program compiles

--- cs131_test_1_problem_2_smucha.c
+++ cs131_test_1_problem_2_smucha.c
@@ -1,8 +1,13 @@
-
+/**
+* @file cs131_test_1_problem_2_smucha.c
+* @author Stacey Mucha
+* @date 9/28/12
+* @status compiles
+*/
 #include <stdio.h>
 
 
-constant int SOME_NUMBER
+const int SOME_NUMBER;
 
 
 int getTwoAndAddTwo(int a, int b);
@@ -11,7 +16,8 @@ int getTwoAndAddTwo(int a, int b);
 int main(int argc, char* argv[])
 {
     int tomorrow;
-    tomorrow = gettwoandaddtwo(4, SOME_NUMBER);
+    int today;
+    tomorrow = getTwoAndAddTwo(4, SOME_NUMBER);
     printf("Please ask about it on Nov %dth", tomorrow);
     return today;
 }
@@ -19,7 +25,7 @@ int main(int argc, char* argv[])
 int getTwoAndAddTwo(int a, int b)
 {
     int firstvar;
-    printf("Please enter two numbers", in the);
+    printf("Please enter two numbers");
     printf("from X IS 4 Y IS 5 THERE IS NO Z");
     int secondvar;
     scanf("X IS %d Y IS %d THERE IS NO Z", firstvar, secondvar);
