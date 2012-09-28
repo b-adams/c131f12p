f202c1f9318d88175c2ccc8c29f468887ff2e94b
Larry <Ltforce12@wells.edu>
2012-09-28 19:31:27 -0400

    documented

--- cs131_test_1_problem_2_lforce.c
+++ cs131_test_1_problem_2_lforce.c
@@ -1,12 +1,34 @@
+/**
+@file test1.list2.c
+@author Prof Adams
+@date 9/28/2012
+*/
+
 
 #include <stdio.h>
+
+/**
+ *@brief Modifier to be added to any date
+ */
 
 const int SOME_NUMBER ;
 
 
+/**
+ *@brief Entry point for the program
+ *@param a First program input to add in
+ *@param b Second program input to add in
+ *@sideeffect Prompts user to enter two numbers in a specific format
+ *@returns The sum of a , b , and two user inputs
+ */
 
 int getTwoAndAddTwo(int a , int b) ;
 
+/**
+ *@brief Entry point for the program
+ *@sideeffect add four four numbers, and then ask the user to ask again in November
+ *@returns 0 if all went well
+ */
 
 int main(void)
 {
