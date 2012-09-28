322b337ec260870f3af45415a1583ef9439b0d0b
Nathan Mitchell <ncmitchell12@wells.edu>
2012-09-28 12:56:58 -0400

    made code 2 with docummentaion

--- cs131_test_1_problem_2_nmitchell.c
+++ cs131_test_1_problem_2_nmitchell.c
@@ -1,13 +1,30 @@
+/**
+ *@file test1.list2.c
+ *@author Prof Adams
+ *@date 9/28/2012
+ */
 
 #include<stdio.h>
 
-
+/**
+ *@brief modifier to be added to any date
+ */
 int SOME_NUMBER;
 
-
+/**
+ *@brief entry point for the program
+ *@sideeffect add four numbers, and then ask the user to ask again in Novemeber
+ *return 0 if all went well
+ */
 int getTwoAndAddTwo(int a, int b);
 
-
+/**
+ *@brief Add two program inputs to two user inputs
+ *@param a first program input to add in
+ *@param b second program input to add in
+ *@sideeffect prompts user to enter two numbers in a specific format
+ *@return the sum of a, b, and two user inputs
+ */
 int main(int argc, char* argv[])
 {
     int tomorrow;
