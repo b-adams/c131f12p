7b73676c617c43b220a8640a036744279316c6c7
Nathan Mitchell <ncmitchell12@wells.edu>
2012-09-28 12:04:16 -0400

    finished todo 1 redo with rest of code

--- cs131_test_1_problem_1_nmitchell.c
+++ cs131_test_1_problem_1_nmitchell.c
@@ -1,4 +1,35 @@
 #include <stdio.h>
 
 const float C_TO_F_MULTIPLE = 1.8;
-const C_TO_F_SHIFT = 32;
+//todo 1
+
+void displayCelsiusInFahrenheit(int tempInC);
+int getBoundedNumber(int minsize, int maxsize);
+
+int main(int argc, char* argv[])
+{
+        int tempToConvert;
+    //todo 3
+    //todo 7
+    //todo 5
+    //todo 7 (again)
+    return 0;
+}
+
+void displayCelsiusInFahrenheit(int tempInC)
+{
+    //todo 2
+    //todo 4
+    //todo 6
+}
+
+int getBoundedNumber(int minsize, int maxsize)
+{
+    int userInput;
+    do {
+        printf("Please enter a number between %d and %d \n", minsize, maxsize);
+        scanf("%d", &userInput);
+    }   while(userInput < minsize || userInput > maxsize);
+    
+    return userInput;
+}
