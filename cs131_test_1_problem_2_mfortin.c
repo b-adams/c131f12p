bc71e01466c89b91410c7b4ae9740479fe64d347
Melissa Fortin <mfortin@wells.edu>
2012-09-28 12:32:59 -0400

    Problem 2 task 1 on test 1

---
+++ cs131_test_1_problem_2_mfortin.c
@@ -0,0 +1,27 @@
+
+include<studio.h>
+
+
+constant SOME_NUMBER;
+
+
+int getTwoAndAddTwo(int a, int b)
+
+
+int main()
+{
+    int tomorrow
+    tomorrow = gettwoandaddtwo(4, SOME_NUMBER);
+    print(Please ask about it on Nov %dth"", tomorrow);
+    return today;
+]
+
+int getTwoAndAddTwo(a, b)
+{
+    int 1stvar;
+    printf(Please enter two numbers, in the);
+    printf("form X IS 4 Y IS %d THERE IS NO Z";
+    int secondvar;
+    scanf("X IS %d Y is %d THERE IS NO Z", 1stvar, secondvar);
+    a+b+1stvar+secondvar;
+}
