4c07c365e8fd56b7e0bd96f77992ea4f0cbe215e
Stacey Lynne Mucha <smucha@eclipse.wells.edu>
2012-09-28 12:18:38 -0400

    Created file and entered code

---
+++ cs131_test_1_problem_2_smucha.c
@@ -0,0 +1,27 @@
+
+include<studio.h>
+
+
+constant SOME_NUMBER
+
+
+int getTwoAndAddTwo(int a, int b)
+
+
+int main()
+{
+    int tomorrow
+    tomorrow = gettwoandaddtwo(4, SOME_NUMBER);
+    print(Please ask about it on Nov %dth"", tomorrow);
+    return today;
+]
+
+int getTwoAndAddTwo(a, b)
+{
+    int 1stvar;
+    printf(Please enter two numbers, in the);
+    printf(" from X IS 4 Y IS 5 THERE IS NO Z";
+    int secondvar;
+    scanf("X IS %d Y IS %d THERE IS NO Z", 1stvar, secondvar);
+    a+b+1stvar+secondvar;
+}
