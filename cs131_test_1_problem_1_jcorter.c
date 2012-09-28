19b45c952f545e1d743bce9074b958ba4399cce5
jessica Corter <jessica corter>
2012-09-28 11:48:58 -0400

    entered the code from listing one on page 3

---
+++ cs131_test_1_problem_1_jcorter.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+const float C_TO_F_MULTIPLE = 1.8;
+//Todo 1
+
+void displayCelsiusInFahrenheit (int tempInC);
+int getBoundedNumber (int minsize, int maxsize);
+
+int main (int argc, char* argv [])
+{
+        int tempToConvert;
+        //todo 3
+        //todo 7
+        //todo 5
+        //Todo 7 again
+        return 0;
+}
+
+void displayCelsiusInFahrenheit (int tempInC)
+{
+    //todo 2
+    //todo 4
+    //todo 6
+}
+
+int getBoundedNumber (int minsize, int maxsize)
+{
+    int userInput
+    do {
+        printf ("please enter a number between %d and %d/n", minsize, maxsize);
+        scanf ("%d", &userInput);
+    }   while (userInput < minsize II userInput > maxsize);    
+
+    return userInput;
+}
