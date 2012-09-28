e34539e678ff3c5c605a93f300071d59a87d7ef7
Zakarra Butts <zmbutts12@wells.edu>
2012-09-28 15:29:26 -0400

    fixed errors on program, runs now

--- cs131_test_1_problem_1_zbutts.c
+++ cs131_test_1_problem_1_zbutts.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
-const float C_TO_MULTIPLY = 1.8;
+const float C_TO_F_MULTIPLE = 1.8;
 const C_TO_F_SHIFT = 32;
 
-void displayCelsiusInFahrenheit(int tempInC)
+void displayCelsiusInFahrenheit(int tempInC);
 int getBoundedNumber(int minsize, int maxsize); //Definition not shown
 
 int main(int argc, char* argv[])
 {
         int tempToConvert;
-    tempToCOnvert = -40;
-    displayCelsiusInFahrenheit=tempToConvert
+    tempToConvert = -40;
+    tempToConvert=displayCelsiusInFahrenheit;
     tempToConvert = (-40, 50);
-    displayCelsiusInFahrenheit=tempToConvert
+    tempToConvert=displayCelsiusInFahrenheit;
     return 0;
 }
 
-voit displayCelsiusInFahrenheit(int tempInC)
+void displayCelsiusInFahrenheit(int tempInC)
 {
     int tempInF;
-    tempInF = tempInC*C_TO_F_MULTIPLE+C_TO_F_SHIFT
+    tempInF = tempInC*C_TO_F_MULTIPLE+C_TO_F_SHIFT;
     printf("\tXCelsius would be Y Fahrenheit", tempInC, tempInF);
 }
