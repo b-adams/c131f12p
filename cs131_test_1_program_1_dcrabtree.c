831a19a3d736cb080359de0fd980519a4384b3f6
Danielle Crabtree <dcrabtree@wells.edu>
2012-09-28 16:14:22 -0400

    Chnaged semi-colons to get program to compile

--- cs131_test_1_program_1_dcrabtree.c
+++ cs131_test_1_program_1_dcrabtree.c
@@ -3,35 +3,34 @@
 const float C_TO_F_MULTIPLE = 1.8;
 const int C_TO_F_SHIFT = 32;
 
-
-void displayCelsiusInFahrenheit (int tempInC)
+void displayCelsiusInFahrenheit(int tempInC);
 int getBoundedNumber (int minsize, int maxsize);
 
-int main (int argc, char* argv [])
+int main(int argc, char* argv [])
 {
     int tempToConvert;
     tempToConvert = -40;
-    displayCelciusInFahrenheit (int tempToConvert)
+    void displayCelciusInFahrenheit(int tempToConvert);
     tempToConvert = getBoundedNumber (-40, 50);
-    displayCelciusInFahrenheit (int tempToConvert)
+    void displayCelciusInFahrenheit(int tempToConvert);
     return 0;
 }
 
 void displayCelciusInFahrenheit (int tempInC)
 {
     int tempInF;
-    tempInF = (tempInC * C_TO_F_MULTIPLE)+ C_TO_F_SHIFT;
+    tempInF = ((tempInC * C_TO_F_MULTIPLE)+ C_TO_F_SHIFT);
     printf("%d Celcius would be %d Fahrenheit.\n", tempInC, tempInF);
 }
 
-int get BoundedNumber (int minsize, int maxsize)
+int getBoundedNumber (int minsize, int maxsize)
 {
     int userInput;
     do {
         printf("Please enter a number between %d and %d\n",
                minsize, maxsize);
-        scanf "%d", &userInput);
-    }while (userInput < minsize || userInput > maxsize);
+        scanf("%d", &userInput);
+    } while(userInput < minsize || userInput > maxsize);
     
     return userInput;
 }
