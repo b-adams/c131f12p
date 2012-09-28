1220ccd090464b5046940c3080ffd4ec367cd39e
Zakarra Butts <zmbutts12@wells.edu>
2012-09-28 15:36:46 -0400

    final of problem 1

--- cs131_test_1_problem_1_zbutts.c
+++ cs131_test_1_problem_1_zbutts.c
@@ -1,7 +1,14 @@
+/**
+*@file cs131_test_1_problem_1_zbutts.c
+*@author Zakarra Butts
+*@date September 28, 2012
+*@brief converting Celsius to Fahrenheit
+*/
+
 #include <stdio.h>
 
 const float C_TO_F_MULTIPLE = 1.8;
-const C_TO_F_SHIFT = 32;
+const float C_TO_F_SHIFT = 32;
 
 void displayCelsiusInFahrenheit(int tempInC);
 int getBoundedNumber(int minsize, int maxsize); //Definition not shown
@@ -10,9 +17,9 @@ int main(int argc, char* argv[])
 {
         int tempToConvert;
     tempToConvert = -40;
-    tempToConvert=displayCelsiusInFahrenheit;
+    tempToConvert = displayCelsiusInFahrenheit;
     tempToConvert = (-40, 50);
-    tempToConvert=displayCelsiusInFahrenheit;
+    tempToConvert = displayCelsiusInFahrenheit;
     return 0;
 }
 
