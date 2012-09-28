a825474511257d73480a4c6b4129b8c3a451bf9c
cprimmer@wells.edu <cprimmer@eclipse.wells.edu>
2012-09-28 12:39:09 -0400

    program with bugs

---
+++ cs131_test_1_problem_2_cprimmer.c
@@ -0,0 +1,27 @@
+
+include <studio.h>
+
+
+constant SOME_NUMBER;
+
+
+int getTwoAndAddTwo(int a,intb)
+
+
+int main()
+{
+    int tomorrow
+    tomorrow=gettwoandaddtwo(4,SOME_NUMBER)
+    print(please ask about in on Nov %dth"", tomorrow);
+    return today;
+]
+
+int getTwoAndAddTwo (a,b)
+{
+    int 1stvar;
+    printf(please enter two numbers, in the);
+    printf(" form X IS 4 Y IS 5 THERE IS NO Z")
+    int secondvar;
+    scanf("X IS %d Y IS %d THERE IS NO Z", 1stvar, secondvar);
+    a+b+1stvar+secondvar;
+}
