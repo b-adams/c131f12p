699d613cec0eec26155ceb68b265e0a2b4b5194b
Chris Tinozzi <ctinozzi@wells.edu>
2012-09-28 12:27:31 -0400

    Documentation added for Problem 2

---
+++ cs131_test_1_problem_2_ctinozzi.c
@@ -0,0 +1,27 @@
+
+include<studio.h>
+
+
+constant SOME_NUMBER;
+
+
+int getTwoAndAddTwo(int a, intb)
+
+
+int main()
+{
+    int tomorrow
+    tomorrow=gettwoandaddtwo(4,SOME_NUMBER);
+    print(Please ask about it on Nov %dth"", tomorrow);
+    return today;
+]
+
+int getTwoAndAddTwo(a, b)
+{
+    int 1stvar;
+    printf(please enter two numbers, in the);
+    printf(" form X IS 4 Y IS 5 THERE IS NO Z");
+    int secondvar;
+    scanf("X IS %D Y IS %D THERE IS NO Z", 1stvar, secondvar);
+    a+b+1stvar+secondvar;
+}
