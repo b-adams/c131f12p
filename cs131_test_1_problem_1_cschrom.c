a301e77125f9e61a0c4da220793f1764a87efa9d
Colby Schrom <cschrom12@wells.edu>
2012-09-28 12:22:09 -0400

    Complete

--- cs131_test_1_problem_1_cschrom.c
+++ cs131_test_1_problem_1_cschrom.c
@@ -1,35 +1,43 @@
+/**
+ *@file cs131_test_1_problem_1_cschrom.c
+ *@author Colby Schrom
+ *@date 9/26/12
+ *@status complete
+ *@brief converts Celsius to Fahrenheit
+ */
+
 #include <stdio.h>
 
 const float C_TO_F_MULTIPLE = 1.8;
-const int C_TO_F_SHIFT = 32
+const int C_TO_F_SHIFT = 32;
 
-void displayCelsiusInFahrenheit(int tempInC)
-int getBoundedNumber9int minsize, int maxsize0;
+void displayCelsiusInFahrenheit(int tempInC);
+int getBoundedNumber(int minsize, int maxsize);
 
-int main(int argc, char* argv[]);
+int main(int argc, char* argv[])
 {
         int tempToConvert;
     tempToConvert = -40;
-    displayCelciusInFahrenheit(tempToConvert);
+    displayCelsiusInFahrenheit(tempToConvert);
     tempToConvert = getBoundedNumber(-40,50);
-   displayCelciusInFahrenheit(tempToConvert);
+    displayCelsiusInFahrenheit(tempToConvert);
     return 0;
 }
 
-void displayCelsiusInFahrenheit(int tempInC);
+void displayCelsiusInFahrenheit(int tempInC)
 {
     int tempInF;
-    int tempInF = tempInC*C_TO_F_MULTIPLE + C_TO_F_SHIFT
+    tempInF = tempInC*C_TO_F_MULTIPLE + C_TO_F_SHIFT;
     printf("%d Celcius would be %d Fahrenheit", tempInC, tempInF);
 }
 
-int getBoundedNumber(int minsize, int maxsize);
+int getBoundedNumber(int minsize, int maxsize)
 {
     int userInput;
     do {
-        printf("Please enter a number between %d and %d\n', minsize, maxsize");
+        printf("Please enter a number between %d and %d\n", minsize, maxsize);
         scanf("%d", &userInput);
-    } while userInput < minsize || userInput > maxsize);
+    } while (userInput < minsize || userInput > maxsize);
     
     return userInput;
 }
