8d76399b7b05a706527357052181baf6625321a3
jessica Corter <jessica corter>
2012-09-28 12:22:13 -0400

    Entered code from problem 2

---
+++ cs131_test_1_problem_2_jcorter.c
@@ -0,0 +1,27 @@
+
+include<stdio.h>
+
+
+constant SOME_NUMBER;
+
+
+int getTwoAndAddTwo(int a, int b)
+
+
+int main()
+{
+    int tomorrow
+    tomorrow=gettwoandaddtwo(4, SOME_NUMBER);
+    print (Please ask about it on Nov %dth "", tomorrow);
+    return today;
+}
+
+int getTwoAndAddTwo(a,b)
+{
+    int 1stvar:
+    printf (Pleasee enter two numbers, in the):
+    printf (" form X IS 4 Y IS 5 THERE IS NO Z";
+    int secondvar;
+    scanf("X IS 5d Y is %d THERE IS NO Z", 1stvar, secondvar);
+    a+b+1stvar+secondvar;
+}
