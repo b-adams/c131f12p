f698c9b3bdf71d3ef8140fd699417af0225af22f
Zakarra Butts <zmbutts12@wells.edu>
2012-09-28 15:19:08 -0400

    did todo 3

--- cs131_test_1_problem_1_zbutts.c
+++ cs131_test_1_problem_1_zbutts.c
@@ -9,7 +9,7 @@ int getBoundedNumber(int minsize, int maxsize); //Definition not shown
 int main(int argc, char* argv[])
 {
         int tempToConvert;
-    //Todo 3
+    tempToCOnvert = -40;
     //Todo 7
     //Todo 5
     //Todo 7 (again)
