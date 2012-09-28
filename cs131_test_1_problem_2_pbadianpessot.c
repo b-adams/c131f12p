3de74d69a2920ff0e97071956a7917b0cff5c741
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-09-28 12:56:28 -0400

    Prog 2 errors corrected

--- cs131_test_1_problem_2_pbadianpessot.c
+++ cs131_test_1_problem_2_pbadianpessot.c
@@ -1,27 +1,27 @@
-include<studio.h>
+include<stdio.h>
 
 
 
-constant SOME_NUMBER;
+const int SOME_NUMBER;
 
 
 int getTwoAndAddTwo(int a, int b);
 
 
-int main()
+int main(int argc, char* argv[])
 {
     int tomorrow;
-    tomorrow = gettwoandaddtwo(4, SOME_NUMBER);
-    printf(Please ask about it on Nov %dth"", tomorrow);
+    tomorrow = getTwoAndAddTwo(4, SOME_NUMBER);
+    printf("Please ask about it on Nov %dth", tomorrow);
     return today;
-]
+}
 
 int getTwoAndAddTwo(a, b)
 {
     int 1stvar;
-    printf(Please enter two numbers, in the);
+    printf("Please enter two numbers, in the");
     printf(" form X IS 4 IS 5 THERE IS NO Z");
     int secondvar;
     scanf("X IS %d Y IS %d THERE IS NO Z", 1stvar, secondvar);
-    a+b+1stvar+secondvar;
+    return a+b+1stvar+secondvar;
 }
