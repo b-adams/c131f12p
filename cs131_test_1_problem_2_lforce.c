a63c699fa3cd24f6a18070214eb8d5148db68559
Larry <Ltforce12@wells.edu>
2012-09-28 19:16:06 -0400

    Compiles

--- cs131_test_1_problem_2_lforce.c
+++ cs131_test_1_problem_2_lforce.c
@@ -1,27 +1,28 @@
 
-include <studio.h>
+#include <stdio.h>
 
-constant SOME_NUMBER ;
+const int SOME_NUMBER ;
 
 
+
+int getTwoAndAddTwo(int a , int b) ;
+
+
+int main(void)
+{
+    int today ;
+    int tomorrow ;
+    tomorrow = getTwoAndAddTwo(4, SOME_NUMBER) ;
+    printf("Please ask about it on Nov %dth", tomorrow) ;
+    return today ;
+}
 
 int getTwoAndAddTwo(int a , int b)
-
-
-int main()
 {
-    int tomorrow
-    tomorrow = gettwoandaddtwo(4, SOME_NUMBER) ;
-    print(Please ask about it on Nov %dth"", tomorrow) ;
-    return today ;
-]
-
-int getTwoAndAddTwo(a, b)
-{
-    int 1stvar ;
-    printf(Please enter two numbers, in the) ;
+    int firstvar ;
+    printf("Please enter %s two numbers", "in the") ;
     printf(" form X IS 4 Y IS 5 THERE IS NO Z") ;
     int secondvar ;
-    scanf("X IS %d Y IS %d THERE IS NO Z" , 1stvar , secondvar) ;
-    a+b+1stvar+secondvar ;
+    scanf("X IS %d Y IS %d THERE IS NO Z" , &firstvar , &secondvar) ;
+    return a+b+firstvar+secondvar ;
 }
