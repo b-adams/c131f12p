ab83bf67c5764f1ae0786510d0d5d2b1cf7eb784
jessica Corter <jessica corter>
2012-09-28 12:44:23 -0400

    added documentation

--- cs131_test_1_problem_2_jcorter.c
+++ cs131_test_1_problem_2_jcorter.c
@@ -1,12 +1,32 @@
+/**
+ *@file cs131_test_1_problem_2_jcorter.c
+ *@author Jessica Corter
+ *@date 9/28/2012
+*/
 
 #include<stdio.h>
 
+/**
+ *@brief Modifier to be added to any date
+ */
 
 const int SOME_NUMBER;
 
+/**
+ *@brief Add two program inputs to two user inputs
+ *@param a First program input to add in
+ *@param b Second program input to add in
+ *@sideeffect Prompts user to enter two numbers in a specific format
+ *@returns The sum of a, b, and two user inputs
+ */
 
 int getTwoAndAddTwo(int a, int b);
 
+/**
+ *@brief Entry point for the program
+ *@sideeffect Add four numbers, and then asl the iser to ask again in November
+ *@returns 0 if all went well
+*/
 
 int main(int argc, char* argv [])
 {
