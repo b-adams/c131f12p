56f4a54488fd89708122587e802628b912403cd7
Zakarra Butts <zmbutts12@wells.edu>
2012-09-28 15:15:45 -0400

    created initial file of problem 1

---
+++ cs131_test_1_problem_1_zbutts.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+const float C_TO_MULTIPLY = 1.8;
+//Todo 1
+
+void displayCelsiusInFahrenheit(int tempInC)
+int getBoundedNumber(int minsize, int maxsize); //Definition not shown
+
+int main(int argc, char* argv[])
+{
+        int tempToConvert;
+    //Todo 3
+    //Todo 7
+    //Todo 5
+    //Todo 7 (again)
+    return 0;
+}
+
+voit displayCelsiusInFahrenheit(int tempInC)
+{
+    //Todo 2
+    //Todo 4
+    //Todo 6
+}
