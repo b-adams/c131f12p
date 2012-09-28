73f3b9731ed207bb3aaca2c8d2a72c7902c230d5
Danielle Crabtree <dcrabtree@wells.edu>
2012-09-28 16:48:22 -0400

    Done debugging the program

--- cs131_test_1_program_2_dcrabtree.c
+++ cs131_test_1_program_2_dcrabtree.c
@@ -2,9 +2,8 @@
 #include <stdio.h>
 
 
-constant SOME_NUMBER = 5;
-
-int getTwoAndAddTwo(int a, int b)
+const int SOME_NUMBER = 5;
+int getTwoAndAddTwo(int a, int b);
 
 
 
@@ -18,10 +17,10 @@ int main (int argc, char* argv[])
 
 int getTwoAndAddTwo (int a, int b)
 {
-    int a;
-    int b;
+    int firstVar;
+    int secondVar;
     printf("Please enter two number, in the\n");
     printf("form X IS 4 Y IS 5 THERE IS NO Z\n");
-    scanf ("%d, %d", &a, &b);
-    return 0;
+    scanf("X IS %d Y IS %d THERE IS NO Z", firstVar, secondVar);
+    return a+b+firstVar+secondVar;
 }
