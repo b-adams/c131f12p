13c289f301e8b432b8c1a37cd726cbd7259dcac8
Danielle Crabtree <dcrabtree@wells.edu>
2012-09-28 16:57:41 -0400

    Added the documentation blocks AND fixed the first return to 0 instead of tomorrow or today

--- cs131_test_1_program_2_dcrabtree.c
+++ cs131_test_1_program_2_dcrabtree.c
@@ -1,19 +1,41 @@
+/**
+ @file test1.list2.c
+ @author Prof. Adams
+ @date 9/28/2012
+*/
 
 #include <stdio.h>
 
+/**
+ @brief Modifier to be added to any date
+*/
 
 const int SOME_NUMBER = 5;
+
 int getTwoAndAddTwo(int a, int b);
 
 
+/**
+ *@brief Entry point for the program
+ *@sideeffect Add four numbers, and then ask the user to ask again in November
+ *@return 0 if all went well
+ */
 
 int main (int argc, char* argv[])
 {
     int tomorrow;
     tomorrow = getTwoAndAddTwo(4, SOME_NUMBER);
     printf("Please ask about it on Nov %dth\n", tomorrow);
-    return tomorrow;
+    return 0;
 }
+
+/**
+@brief Add two program inputs to two user inputs
+@param a First program input to add in
+@param b Second input to add in
+@sideeffect Prompts user to enter two numbers in a specific format
+@return The sum of a, b, and two user inputs
+*/
 
 int getTwoAndAddTwo (int a, int b)
 {
