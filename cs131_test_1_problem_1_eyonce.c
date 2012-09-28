72076fc231a04aa1c7ebd7e531b114222a4c124c
eyonce@wells.edu <eyonce@eclipse.wells.edu>
2012-09-28 11:56:23 -0400

    declared and assigned constant

---
+++ cs131_test_1_problem_1_eyonce.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+const float C_TO_F_MULTIPLE = 1.8;
+cont int C_T_F_SHIFT = 32;
+ 
+ void displayCelsiusInFahrenheit(int tempInC);
+ int getBoundedNumber (int minsize, int maxsize);
+ 
+ int main(int argc, char* argv[])
+ {
+        int tempToConvert;
+    //Todo 3
+    //Todo 7
+    //Todo5
+    //Todo 7 (again)
+    return 0;
+ }
+ 
+ void displayCelsiusInFahrenheit (int tempInC)
+ {
+    //Todo 2
+    //Todo4
+    //Todo6
+ }
+ 
+ int getBoundedNumber (int minsize, int maxsize)
+ {
+    int userInput;
+    do {
+      printf("Please enter a number between %d and %d\n",
+             minsize, maxsize);
+      scanf ("%d", &userInput);
+    } while (userInput < insize || userInput > maxsize);
+    
+    return userInput;
+ }
