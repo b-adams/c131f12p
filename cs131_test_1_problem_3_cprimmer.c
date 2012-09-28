0ead2ee0ecde7ba937409e93947d87342350abac
cprimmer@wells.edu <cprimmer@eclipse.wells.edu>
2012-09-28 13:03:20 -0400

    third task

---
+++ cs131_test_1_problem_3_cprimmer.c
@@ -0,0 +1,23 @@
+/**
+ @file test1.list2.c
+ @author Prof Adams
+ @date 9/28/2012
+*/
+
+/**
+ @brief Entry Point for the program
+ @sdeeffect add four number, and the ask the user to ask again in november
+ @returns 0 if all went well
+*/
+
+/**
+ @brief Modifier to be added to any date
+*/
+
+/**
+ @brief add two program inputs to the two user inputs
+ @param a first program input to add in
+ @param b second program input to add in
+ @sideeffect prompts user to enter two numbers in a specific format
+ @returns the sum of a, b, and two user inputs
+*/
