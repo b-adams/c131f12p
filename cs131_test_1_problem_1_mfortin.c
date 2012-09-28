7f00b7be5b8ad68571469113ac0049d165b3453f
Melissa Fortin <mfortin@wells.edu>
2012-09-28 12:01:04 -0400

    I completed todo 3

--- cs131_test_1_problem_1_mfortin.c
+++ cs131_test_1_problem_1_mfortin.c
@@ -8,8 +8,7 @@ int getBoundedNumber(int minsize, int maxsize);
 
 int main(int argc, char* argv[])
 {
-    int tempToConvert;
-    //Todo 3
+    int tempToConvert = -40;
     //Todo 7
     //Todo 5
     //Todo 7
