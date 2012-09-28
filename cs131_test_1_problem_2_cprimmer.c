e23ea85423ae6694a7b891f22ea8ac5a0293269b
cprimmer@wells.edu <cprimmer@eclipse.wells.edu>
2012-09-28 12:43:51 -0400

    errors corrected

--- cs131_test_1_problem_2_cprimmer.c
+++ cs131_test_1_problem_2_cprimmer.c
@@ -1,27 +1,27 @@
 
-include <studio.h>
+#include <studio.h>
 
 
-constant SOME_NUMBER;
+const float SOME_NUMBER;
 
 
-int getTwoAndAddTwo(int a,intb)
+int getTwoAndAddTwo(int a,int b)
 
 
 int main()
 {
-    int tomorrow
-    tomorrow=gettwoandaddtwo(4,SOME_NUMBER)
-    print(please ask about in on Nov %dth"", tomorrow);
-    return today;
-]
+    int (tomorrow);
+    tomorrow=gettwoandaddtwo(int 4, int SOME_NUMBER);
+    printf(please ask about it on Nov %dth"", tomorrow);
+    return (today);
+}
 
-int getTwoAndAddTwo (a,b)
+int getTwoAndAddTwo (int a,int b)
 {
-    int 1stvar;
+    int (1stvar);
     printf(please enter two numbers, in the);
-    printf(" form X IS 4 Y IS 5 THERE IS NO Z")
-    int secondvar;
-    scanf("X IS %d Y IS %d THERE IS NO Z", 1stvar, secondvar);
-    a+b+1stvar+secondvar;
+    printf(" form X IS 4, Y IS 5, THERE IS NO Z")
+    int (secondvar);
+    scanf("X IS %d, Y IS %d, THERE IS NO Z", 1stvar, secondvar);
+    return (a+b+1stvar+secondvar);
 }
