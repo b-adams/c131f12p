7d53398c1b1385e82d372d95ff0aa031300a368a
Melissa Fortin <mfortin@wells.edu>
2012-09-29 12:00:32 -0400

    Problem 2 with all corrections

--- cs131_test_1_problem_2_mfortin.c
+++ cs131_test_1_problem_2_mfortin.c
@@ -10,18 +10,18 @@ int getTwoAndAddTwo(int a, int b);
 
 int main(int argc, char* argv[])
 {
-    int tomorrow();
-    tomorrow = getTwoAndAddTwo(4, const int SOME_NUMBER);
-    printf("Please ask about it on Nov %d th\n", int tomorrow);
-    return tomorrow();
+    int tomorrow;
+    tomorrow = getTwoAndAddTwo(4, SOME_NUMBER);
+    printf("Please ask about it on Nov %d th\n", tomorrow);
+    return tomorrow;
 }
 
 int getTwoAndAddTwo(int a, int b)
 {
-    int 1stvar;
-    printf("Please enter two numbers in the\n");
-    printf("form X IS 4 Y IS %d THERE IS NO Z\n)";
-    int 2ndvar();
-    scanf("X IS %d Y is %d THERE IS NO Z\n", 1stvar, 2ndvar);
-    return a+b+1stvar+2ndvar;
+    int firstvar;
+    printf("Please enter two numbers, in the");
+    printf("form X IS 4 Y IS 5 THERE IS NO Z\n");
+    int secondvar;
+    scanf("X IS %d Y is %d THERE IS NO Z\n", firstvar, secondvar);
+    return a+b+firstvar+secondvar;
 }
