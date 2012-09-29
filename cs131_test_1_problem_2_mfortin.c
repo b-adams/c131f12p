439a7d8150a354a31225c10023b9d59af410fb3b
Melissa Fortin <mfortin@wells.edu>
2012-09-29 12:05:01 -0400

    Program now has documentation

--- cs131_test_1_problem_2_mfortin.c
+++ cs131_test_1_problem_2_mfortin.c
@@ -1,13 +1,24 @@
+/**
+ @file test1.list2.c
+ @author Prof Adams
+ @date 9/28/2012
+ */
 
 #include<stdio.h>
 
-
+/**
+ @brief Modifier to be added to any date
+ */
 const int SOME_NUMBER;
 
 
 int getTwoAndAddTwo(int a, int b);
 
-
+/**
+ @brief Entry point for the program
+ @sideeffect Add four numbers, and then ask the user to ask again in November
+ @returns 0 if all went well
+ */
 int main(int argc, char* argv[])
 {
     int tomorrow;
@@ -16,6 +27,13 @@ int main(int argc, char* argv[])
     return tomorrow;
 }
 
+/**
+ @brief Add two program inputs to two user inputs
+ @param a First program input to add in
+ @param b Second program input to add in
+ @sideeffect Promps user to enter two numbers in a specific format
+ @returns The sum of a, b, and two user inputs
+ */
 int getTwoAndAddTwo(int a, int b)
 {
     int firstvar;
