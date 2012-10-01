744d2f04112c29b1d7f146e6ed49553aca51a4e7
Stacey Lynne Mucha <smucha@eclipse.wells.edu>
2012-10-01 14:09:41 -0400

    Fixed program so it prints the right answer

--- cs131_program_3b_smucha.c
+++ cs131_program_3b_smucha.c
@@ -5,10 +5,10 @@ int main(void)
   int moneyfordinner;
      printf("How much money do you have for dinner?");
      scanf("%d", &moneyfordinner); 
-     if(moneyfordinner > 50) //checking whether two integers are equal.
+     if(moneyfordinner > 50) 
           printf("You should go to the Fargo!"); 
      else 
-        if(50 > moneyfordinner > 20) //checking whether numb1 is greater than numb2. 
+        if(moneyfordinner > 20) 
           printf("You should got to the Backstage Grill!"); 
         else 
           printf("You should just stick to the Dining Hall"); 
