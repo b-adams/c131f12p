5906fb285cd51a23ca606e404276adc0b0c2aed4
eyonce@wells.edu <eyonce@eclipse.wells.edu>
2012-10-02 20:13:24 -0400

    created and documented integer constant

--- cs131_program_2_eyonce.c
+++ cs131_program_2_eyonce.c
@@ -7,6 +7,16 @@
 
 #include <stdio.h>
 
+/**
+ @brief The answer that the trick always comes up with
+ 
+ For not-very-mysterious algebraic reasons, this trick will always come up
+ with the same answer. Rather than have '2' randomly show up in our code as
+ a 'magic number', we give it a name here.
+ */
+const int THE_ANSWER_EVERY_TIME;
+
+
 {
 int main (argc, char* argv[])
 return 0;
