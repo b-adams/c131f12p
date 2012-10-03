016490f62024a61f4e31ba23b8b22530d1372826
Danielle Crabtree <dcrabtree@wells.edu>
2012-10-03 12:43:09 -0400

    The final code of the program but documentation has yet to be added

--- cs131_program_3b_dcrabtree.c
+++ cs131_program_3b_dcrabtree.c
@@ -7,7 +7,7 @@
 
 #include <stdio.h>
 
-char whereToEat (int money)
+char whereToEat (int money);
 
 int main (void)
 {
@@ -16,24 +16,15 @@ int main (void)
     
     printf("Please enter how much money you have for dinner: ");
     scanf("%d", &money);
-    
-    restuarant= whereToEat(money);
-    printf("You should eat at the %c\n", restuarant);
+  
+    if(money >= 50)
+        printf("You should go to the Fargo!\n");
+    else if (money < 50 && money>= 20)
+        printf("You should go to the Backstage Grill!\n");
+    else if(money < 20)
+        printf("Maybe you should just stick to the Wells Dining Hall\n");
     
     return 0;
-}//main
+}// main
 
-char whereToEat (int money)
-{
-    char restuarant;
-    
-    if(money >= 50)
-        restuarant= 'Go to the Fargo!';
-    else if (money < 50 && money>= 20)
-        restuarant=  'Go to the Backstage Grill';
-    else if (money < 20)
-        restuarant= 'Maybe you should just stick to the Wells Dining Hall';
-    
-    return restuarant;
-}//whereToEat
 
