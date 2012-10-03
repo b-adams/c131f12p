7660d672d4990cdcb2a8ba51c71d3495f263f960
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-10-03 15:13:30 -0400

    prog 3f initial code

--- cs131_program_3f.pbadianpessot.c
+++ cs131_program_3f.pbadianpessot.c
@@ -7,3 +7,32 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/*
+ * @brief main function
+ * @param argc Number of command-line arguements
+ * @param argv The comman-line tokens that were typed
+ * @returns 0 if no news
+ */
+
+ 
+
+int main(int argc, char* argv[])
+{
+    int userInput;
+    
+    printf("Please input the internal temperature of your steak");
+    scanf("%d", &userInput);
+    if(userInput<=70)
+    {
+        printf("Your steak is raw! \n");
+    }   else    {
+        if(userInput<=145)
+        {
+            printf("Your Steak is undercooked \n");
+        } else  {
+            printf("Your steak is just right! \n");
+        }
+        
+    }
+    return 0;
+}
