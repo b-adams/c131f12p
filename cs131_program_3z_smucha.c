96076ce2aaffdfd3e2c85830b90bd024cf4efe51
Stacey Lynne Mucha <smucha@eclipse.wells.edu>
2012-10-03 12:30:57 -0400

    Wrote program. Testing to see if compiles and runs right

---
+++ cs131_program_3z_smucha.c
@@ -0,0 +1,55 @@
+/**
+@file cs131_program_3z_smucha.c
+@brief Determining tax rate based on age and time in military
+@author Stacey Mucha
+@date October 3, 2012
+@status Writing program
+*/
+
+#include <stdio.h>
+
+
+/**
+@brief main function of program
+@param argc Number of command-line arguments
+@param argv The command-line tokens that were typed
+@returns 0 if works right
+*/
+
+int main(int argc, char* argv[])
+{
+  int userAge;
+  int serviceTime;
+  
+  printf("How old are you?\n");
+  scanf("%d", &userAge); 
+
+  printf("How many years have you served in the military?\n");
+  scanf("%d", &serviceTime);
+  
+  if(userAge < 18) 
+  {
+    printf("You don't have to pay any taxes.\n"); 
+  } else {
+    if(userAge <= 65 & serviceTime < 5) 
+    {
+      printf("You have a tax rate of 50%\n"); 
+    } else {
+      if(userAge <= 65 & serviceTime > 5)
+      {
+        printf("You have a tax rate of 20%\n");
+      } else {
+        if(userAge > 65 & serviceTime < 5)
+        {
+          printf("You have a tax rate of 20%\n");
+        } else {
+          if(userAge > 65 & serviceTime > 5)
+          {
+            printf("You have a tax rate of 10%\n");
+          }
+        }
+      }
+    }
+  }
+return 0;
+}
