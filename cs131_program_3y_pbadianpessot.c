5dbf981e6150dd4730918ee0416937d41dd998b8
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-10-03 16:37:08 -0400

    prog 3y initial code

--- cs131_program_3y_pbadianpessot.c
+++ cs131_program_3y_pbadianpessot.c
@@ -6,3 +6,47 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+
+/*
+ * @brief main function
+ * @param argc Number of command-line arguements
+ * @param argv The comman-line tokens that were typed
+ * @returns 0 if no news
+ */
+
+ 
+
+int main(int argc, char* argv[])
+{
+    int size;
+    int price;
+    int numberProduced;
+    int massive;
+    
+    printf("How big is your car?");
+    scanf("%d", &size);
+    printf("How much does your car cost?");
+    scanf("%d", &price);
+    printf("How many of you car were made?");
+    scanf("%d", &numberProduced);
+    
+    if(price>=10000000)
+    {
+        printf("You're In! \n");
+    }   else    {
+        if(massive>=2000000)
+        {
+            massive=price*size;
+            printf("You're In! \n");
+        } else  {
+                if(collectible>=50000)
+                    {    
+                        collectible=price*numberProduced;
+                        printf("You're In! \n");
+                    }  else {
+                        printf("No Admission");
+            }
+        }
+    }
+    return 0;
+}
