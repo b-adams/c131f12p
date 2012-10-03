9eedf8001546d36a4518443a3cca5e7181460a47
Danielle Crabtree <dcrabtree@wells.edu>
2012-10-03 14:22:14 -0400

    Got rid of the unnecessary varianle char restuarant and added Allman style

--- cs131_program_3b_dcrabtree.c
+++ cs131_program_3b_dcrabtree.c
@@ -2,6 +2,7 @@
  *@file cs131_program_3b_dcrabtree.c
  *@author Danielle Crabtree
  *@date October 1, 2012
+ *@brief Where to Eat Program
  *@status Working!
  */
 
@@ -27,17 +28,22 @@ char whereToEat (int money);
 int main (void)
 {
     int money;
-    char restuarant;
     
     printf("Please enter how much money you have for dinner: ");
     scanf("%d", &money);
   
     if(money >= 50)
+    {
         printf("You should go to the Fargo!\n");
+    }
     else if (money < 50 && money>= 20)
+    {
         printf("You should go to the Backstage Grill!\n");
+    }
     else if(money < 20)
+    {
         printf("Maybe you should just stick to the Wells Dining Hall...\n");
+    }
     
     return 0;
 }// main
