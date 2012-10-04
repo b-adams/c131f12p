509375806237cb2a01e7048fc834df47cc0168e3
cprimmer@wells.edu <cprimmer@eclipse.wells.edu>
2012-10-03 23:33:05 -0400

    program 3e about steaks

---
+++ cs131_program_3e_cprimmer.c
@@ -0,0 +1,28 @@
+/**
+ @file cs131_program_3e_cprimmer.c
+ @author cody primmer
+ @date 10/3/12
+ @status in progress
+ */
+
+#include<stdio.h>
+
+if(tempOfSteak>145)
+{
+    printf("your steak is over cooked.\n")
+}
+else
+if(tempOfSteak=145)
+{
+    printf("your steak is perfect.\n")
+}
+else
+if(tempOfSteak<145)
+{
+    printf("your steak is undercooked.\n")
+}
+else
+if(tempOfSteak=roomtemp=70)
+{
+    printf("your steak is raw sucks.\n")
+}
