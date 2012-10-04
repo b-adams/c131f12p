0fdcf3442bda6b9574ffe32fa350f13ebff938cb
Larry <Ltforce12@wells.edu>
2012-10-04 03:41:09 -0400

    Program ready for grading.

--- cs131_program_3b_lforce.c
+++ cs131_program_3b_lforce.c
@@ -1,11 +1,45 @@
+/**
+ *@file cs131_program_3b_lforce.c
+ *@author Larry Force
+ *@date 10/3/12
+ *@status working
+ */
+
+
 
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ *@author Larry Force
+ *@brief pauses the program and waits for input
+ *@return returns nothing
+ */
+void pause() ;
+
+
+/**
+ *@author Larry Force
+ *@brief walks the user through the steps of the program
+ *@return returns nothing
+ */
+void walkthrough() ;
+
+
+
+/**
+ *@author Larry Force
+ *@brief main function
+ *@param argc the number of options in the command line
+ *@param argv the command line (typed arguments)
+ *@return returns 0
+ */
 int main(int argc, char* argv[])
 {
 
 int cash ;
+
+walkthrough() ;
 
 printf("How much cash do you have?\n") ;
 scanf("%d" , &cash) ;
@@ -26,5 +60,30 @@ if (cash>50)
                     } 
         }
 }
+return 0 ;
+}
 
+void pause()
+{
+    
+    int meaningless ;
+    
+    printf(" Please enter a value and press Enter to continue \n ") ;
+    scanf("%d" , &meaningless) ;
+    
+}
+
+
+void walkthrough()
+{
+        printf("First the program will ask you how much money you have.\n") ;
+    
+    pause() ;
+            printf("Next you will enter how much money you have for spending.\n") ;
+    
+    pause() ;
+            printf("Finally, the program will recommend where to eat based on your money.\n") ;
+    
+    pause() ;
+    
 }
