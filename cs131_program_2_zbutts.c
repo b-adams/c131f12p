07ae2d81f5f052ac2a1dda973aecd8414619f706
Zakarra Butts <zmbutts12@wells.edu>
2012-10-03 21:41:14 -0400

    next step complete

--- cs131_program_2_zbutts.c
+++ cs131_program_2_zbutts.c
@@ -7,13 +7,7 @@
 
 #include <stdio.h>
 
-/**
- *@brief The answer that the trick always comes up with
- *For not-very-mysterious algebraic reasons, this trick will always come up
- *with the same answer. Rather than have '2' randomly show up in our code as
- *a 'magic number', we give it a name here.
- */
-const int THE_ANSWER_EVERY_TIME
+const int THE_ANSWER_EVERYTIME = 2;
 
 /**
  @brief Compute a power of 2
@@ -26,7 +20,17 @@ const int THE_ANSWER_EVERY_TIME
  Otherwise, it returns twice the value of twoToThe(n-1)
  [We'll talk about this 'recursion' trick later in the class. It's very useful.]
  */
+
 int twoToThe(int exponent);
+
+/**
+ @brief Main procedure.
+
+ @param argv the command line (typed arguments)
+ @param argc the number of options in the command line.
+ @return zero if successful
+ @author Nathan Mitchell
+*/
 
 /**
  @brief Double a number
@@ -36,32 +40,41 @@ int twoToThe(int exponent);
  
  Very simple function, should just return 2*n or n+n
  */
+
 int twice(int n);
 
+/**
+ @brief Walk the user through the arithmetic in the trick
+ @param theUsersNumber Whatever number the user picked
+ @sideeffect Displays the computations for each step of the trick.
+ @returns the result of going through the arithmetic of the trick
+ 
+ Displays values of 2^k for the various k, and
+ Walks through subtracting 2^0, multiplying by 2^2, adding 2^3, dividing by 2^1,
+ and then subtracting twice the original number.
+ */
 
-int main (int argc, char* argv[])
-{
-    return 0;
-}
+int trickCheckArithmetic(int theUsersNumber);
 
-int twoToThe(int exponent)
-{
-    int theResult;
-    
-    printf("-> Calling twoToThe(%d)\n",exponent);
-    
-	if(exponent<0) theResult=1/twoToThe(-exponent);
-	else if(exponent==0) theResult=1;
-	else theResult=twice(twoToThe(exponent-1));
-    
-    printf("<- twoToThe(%d) returns %d\n",exponent, theResult);
-    
-    return theResult;
-}
+/**
+ @brief Pause until the user is ready to continue
+ @sideeffect Prompts the user to press enter
+ 
+ After printing a prompt, this function calls the getchar function
+ which will scan for the next letter in the stream. If the user presses
+ enter, a newline will be the next character, getchar will finish, and
+ the rest of the program will continue.
+ */
 
-int twice(int n)
-{
-    int theResult;
-    
-    printf()
-}
+void pause();
+
+/**
+ @brief The answer that the trick always comes up with
+ 
+ For not-very-mysterious algebraic reasons, this trick will always come up
+ with the same answer. Rather than have '2' randomly show up in our code as
+ a 'magic number', we give it a name here.
+ */
+
+void trickInstuctions();
+
