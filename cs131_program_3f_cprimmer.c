3d14531e80eaabc246b5486b8d00e6c0770ef5cc
cprimmer@wells.edu <cprimmer@eclipse.wells.edu>
2012-10-03 23:44:22 -0400

    program 3f

---
+++ cs131_program_3f_cprimmer.c
@@ -0,0 +1,24 @@
+/**
+ @file cs131_program_3f_cprimmer.c
+ @author cody primmer
+ @date 10/3/12
+ @status in progress
+ */
+
+#include<stdio.h>
+
+if(theNumberofTimesyouhaveseenlordoftherings>5)
+{
+    printf("maybe watch it again, for good measure.\n")
+}
+else
+if(theNumberofTimesyouhaveseenlordoftherings>0)
+{
+    printf("you definitely need to see it again.\n")
+}
+else
+if(theNumberofTimesyouhaveseenlordoftherings=0)
+{
+    printf("you need to see it!\n")
+}
+return("remember the popcorn.\n")
