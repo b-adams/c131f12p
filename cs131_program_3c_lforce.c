7e624a8e3c7c597916580c5b7555485002d16b23
Larry <Ltforce12@wells.edu>
2012-10-04 03:51:08 -0400

    Program ready for grading.

--- cs131_program_3c_lforce.c
+++ cs131_program_3c_lforce.c
@@ -1,20 +1,61 @@
+/**
+ *@file cs131_program_3c_lforce.c
+ *@author Larry Force
+ *@date 10/3/12
+ *@status working
+ */
+
+
 
 #include <stdio.h>
 #include <stdbool.h>
 
+/**
+ *@author Larry Force
+ *@brief determines whether the year supplied is odd or even
+ *@return returns nothing
+ */
 void eo(int time) ;
 
+/**
+ *@author Larry Force
+ *@brief this determines whether the year supplied is before or after the school became co-ed
+ *@return returns nothing
+ */
 void date(int colors) ;
+
+/**
+ *@author Larry Force
+ *@brief pauses the program and waits for input
+ *@return returns nothing
+ */
+void pause() ;
+
+
+/**
+ *@author Larry Force
+ *@brief walks the user through the steps of the program
+ *@return returns nothing
+ */
+void walkthrough() ;
 
 bool coEd ;
 
 bool oddeven ;
 
+/**
+ *@author Larry Force
+ *@brief main function
+ *@param argc the number of options in the command line
+ *@param argv the command line (typed arguments)
+ *@return returns 0
+ */
 int main(int argc, char* argv[])
 {
 
 int year ;
 
+walkthrough() ;
 
 printf("What year did you graduate?") ;
 scanf("%d" , &year) ;
@@ -49,6 +90,8 @@ if (coEd)
             printf("\n Hello alumnae, here is your lavender and marigold shirt. \n") ;
             }
     }
+
+return 0 ;
 }
 
 
@@ -88,3 +131,31 @@ void date(int colors)
                 else
                 printf("error") ;
                 }
+
+void pause()
+{
+    
+    int meaningless ;
+    
+    printf(" Please enter a value and press Enter to continue \n ") ;
+    scanf("%d" , &meaningless) ;
+    
+}
+
+
+void walkthrough()
+{
+        printf("First the program will ask what year you graduated.\n") ;
+    
+    pause() ;
+        printf("Next the program will have you input the year you graduated.\n") ;
+    
+    pause() ;
+        printf("The program will then do math in the background to determine if you graduated in a co-ed class and if you were even or odd.\n") ;
+    
+    pause() ;
+        printf("Finally, the program will greet you and state what color shirt you are recieving.\n") ;
+    
+    pause() ;
+    
+}
