b58ffb52ef7a5b6a5599159540ceec1bc8b52597
Zakarra Butts <zmbutts12@wells.edu>
2012-10-05 12:06:47 -0400

    added constants to program

--- cs131_program_4_zbutts.c
+++ cs131_program_4_zbutts.c
@@ -6,3 +6,9 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+
+const int MENU_OPT_EXIT = *;
+const int MENU_OPT_PROG_B = W;
+const int MENU_OPT_PROG_E = a;
+const int MENU_OPT_PROG_F = ?;
+const int MENU_OPT_PROG_H = 2;
