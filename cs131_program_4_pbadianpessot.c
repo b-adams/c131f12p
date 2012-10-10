6ef9582777201e578511e57a2107108f9f55535c
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-10-10 15:07:25 -0400

    Inital function calls of prog 3 functions.

--- cs131_program_4_pbadianpessot.c
+++ cs131_program_4_pbadianpessot.c
@@ -11,31 +11,31 @@
  @brief The option to exit the loop
  */
  
-const char MENU_OPT_EXIT=?;
+const char MENU_OPT_EXIT='?';
 
 /*
  @brief The option to run program 3b
  */
  
-const char MENU_OPT_PROG_B=*;
+const char MENU_OPT_PROG_B='*';
 
 /*
  @brief The option to run program 3e
  */
  
-const char MENU_OPT_PROG_E=W;
+const char MENU_OPT_PROG_E='W';
 
 /*
  @brief The option to run program 3f
  */
  
-const char MENU_OPT_PROG_F=a;
+const char MENU_OPT_PROG_F='a';
 
 /*
  @brief The option to run program 3y
  */
  
-const char MENU_OPT_PROG_Y=2;
+const char MENU_OPT_PROG_Y='2';
 
 /*
  * @brief runs program 3B, telling someone where to eat with their group
@@ -87,3 +87,107 @@ int runProgram(void);
  */
 
 int main(int argc, char* argv[]);
+
+
+int do_prog_3B(void)
+{
+    int userInput;
+    
+    printf("Please input how much you are having for dinner:");
+    scanf("%d", &userInput);
+    if(userInput>=50)
+    {
+        printf("Go to the Fargo \n");
+    }   else    {
+        if(userInput>=20)
+        {
+            printf("Go to the Backstage Grill \n");
+        } else  {
+            printf("Go to the Dining Hall \n");
+        }
+        
+    }
+    return 0;
+}
+
+int do_prog_3E(void)
+{
+    int userInput;
+    
+    printf("How many times have you seen Lord of the Rings? ");
+    scanf("%d", &userInput);
+    if(userInput>=5)
+    {
+        printf("Maybe watch it again, for good measure \n");
+    }   else    {
+        if(userInput==0)
+        {
+            printf("You need to see it!! \n");
+        } else  {
+            printf("You definately need to see it again \n");
+        }
+        
+    }
+    printf("Remeber the popcorn \n");
+    return 0;
+}
+
+
+int do_prog_3F(void)
+{
+    int userInput;
+    
+    printf("Please input the internal temperature of your steak:");
+    scanf("%d", &userInput);
+    if(userInput<=70)
+    {
+        printf("Your steak is raw! \n");
+    }   else    {
+        if(userInput<=145)
+        {
+            printf("Your Steak is undercooked \n");
+        } else  {
+            printf("Your steak is just right! \n");
+        }
+        
+    }
+    return 0;
+}
+
+
+int do_prog_3Y(void)
+{
+    int size;
+    int price;
+    int numberProduced;
+    int massive;
+    int collectible;
+    
+    printf("How big is your car?");
+    scanf("%d", &size);
+    printf("How much does your car cost?");
+    scanf("%d", &price);
+    printf("How many of you car were made?");
+    scanf("%d", &numberProduced);
+    
+    if(price>=10000000)
+    {
+        printf("You're In! \n");
+    }   else    {
+        if(massive>=2000000)
+        {
+            massive=price*size;
+            printf("You're In! \n");
+        } else  {
+                if(collectible>=50000)
+                    {    
+                        collectible=price*numberProduced;
+                        printf("You're In! \n");
+                    }  else {
+                        printf("No Admission \n");
+            }
+        }
+    }
+    return 0;
+}
+
