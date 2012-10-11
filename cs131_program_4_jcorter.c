900f938edbcb7d2279a503d6b5b2dbc4009db3c4
jessica Corter <jessica corter>
2012-10-10 21:27:54 -0400

    added main, runProgram and getChoice

--- cs131_program_4_jcorter.c
+++ cs131_program_4_jcorter.c
@@ -31,3 +31,124 @@ const char MENU_OPT_PROG_D = *
 */
 const char MENU_OPT_PROG_F = ? 
 
+/**
+ * @brief
+ * @param
+ * @param
+ * @sideeffect
+ * @returns
+*/
+bool runProgram (char whichProg);
+
+
+/**
+ * @brief
+ * @param
+ * @param
+ * @sideeffect
+ * @returns
+*/
+char getChoice ();
+
+/**
+ * @brief
+ * @param
+ * @param
+ * @sideeffect
+ * @returns
+*/
+void do_prog_3B ();
+
+/**
+ * @brief
+ * @param
+ * @param
+ * @sideeffect
+ * @returns
+*/
+void do_prog_3C ();
+
+/**
+ * @brief
+ * @param
+ * @param
+ * @sideeffect
+ * @returns
+*/
+void do_prog_3D ();
+
+/**
+ * @brief
+ * @param
+ * @param
+ * @sideeffect
+ * @returns
+*/
+void do_prog_3F ();
+
+/**
+ * @brief Main procedure
+ * @returns Zero if successful
+ * @author Jessica Corter
+*/
+int main (void) //int main(int argc, char* argv[])
+{
+    char selection;
+    selection = getChoice();
+    while (selection != MENU_OPT_EXIT)
+    {
+        runProgram (selection);
+        selection = getChoice();
+    }
+    printf ("Goodbye/n");
+    return void;
+}
+
+bool runProgram(char whichProg)
+{
+    char whichProg;
+    Printf("Please enter your selection: \n");
+    scanf( " %c", &whichProg);
+    switch(whichProg)
+    {
+        case MENU_OPT_PROG_3B;
+            printf("Running Program 3B\n");
+            do_prog_3B();
+            Printf("Program 3B Done\n\n");
+        
+        case MENU_OPT_PROG_3C;
+            printf("Running Program 3C\n");
+            do_prog_3C();
+            Printf("Program 3C Done\n\n");
+            
+        case MENU_OPT_PROG_3D;
+            printf("Running Program 3D\n");
+            do_prog_3D();
+            Printf("Program 3D Done\n\n");
+            
+        case MENU_OPT_PROG_3F;
+            printf("Running Program 3F\n");
+            do_prog_3F();
+            Printf("Program 3F Done\n\n");
+            
+        default: printf("Invalid selection: [%c]\n", whichProg);
+            
+        return False;      
+    }
+    return True;    
+}
+
+char getChoice()
+{
+    char getChoice;
+    printf("Main Menu:\n");
+    printf("\t[%c] Run program 3B\n", MENU_OPT_PROG_3B);   
+    printf("\t[%c] Run program 3C\n", MENU_OPT_PROG_3C);
+    printf("\t[%c] Run program 3D\n", MENU_OPT_PROG_3D);
+    printf("\t[%c] Run program 3F\n", MENU_OPT_PROG_3F);
+    printf("Please enter your selection, or %c to quit: ", MENU_OPT_EXIT);
+    scanf(" %c", &userChoice);
+    return userChoice;
+
+}
+
