a455f33b974e3e13d673a1f77c0e1e09b2a9517f
Eric Shane <ewshane@hotmail.com>
2012-10-10 23:48:31 -0400

    Compiles and runs thanks from help with Colby and Larry.

--- cs131_program_4_ESHANE.c
+++ cs131_program_4_ESHANE.c
@@ -1,63 +1,154 @@
-# include <stdio.h>
-# include <stdbool.h>
+/**
+ *@file cs131_program_4_ESHANE.c
+ *@author Eric Shane
+ *@date 10/9/12
+ *@brief Menuizing program 3
+ *@status works
+ */
+
+#include <stdio.h>
+
+#include <stdbool.h>
+
+/**
+ *@brief used as a value to exit program
+ */
+const char MENU_OPT_EXIT = '?';
+
+/**
+ *@brief the selection of program B
+ */
+const char MENU_OPT_PROG_B = '*';
+
+/**
+ *@brief the selection of program C
+ */
+const char MENU_OPT_PROG_C = 'W';
+
+/**
+ *@brief the selection of program E
+ */
+const char MENU_OPT_PROG_E = 'a';
+
+/**
+ *@brief the selection of program F
+ */
+const char MENU_OPT_PROG_F = '2';
+
+/**
+ *@brief runs whichever program is selected
+ *@param is the value of the user's selection
+ *@return returns true
+ */
+bool runProgram(char whichProg) ;
+
+char getChoice() ;
+
+/**
+ *@author Eric Shane
+ *@brief runs program 3B
+ *@return returns 0
+ */
+
+void do_prog_3B() ;
+
+/**
+ *@author Eric Shane
+ *@brief runs program 3C
+ *@return returns 0
+ */
+
+void do_prog_3C() ;
+
+/**
+ *@author Eric Shane
+ *@brief runs program 3E
+ *@return returns 0
+ */
+
+void do_prog_3E() ;
+
+/**
+ *@author Eric Shane
+ *@brief runs program 3F
+ *@return returns 0
+ */
+
+void do_prog_3F() ;
+
+/**
+ *@author Eric Shane
+ *@brief pauses program
+ *@return returns 0
+ */
+
+void pause() ;
 
 
-const int MENU_OPT_EXIT = 2;
-const char MENU_OPT_PROG_B = '?';
-const char MENU_OPT_PROG_C = 'a';
-const char MENU_OPT_PROG_F = '*';
-const char MENU_OPT_PROG_H = 'W';
 
-char getChoice();
-bool runProgram(char whichProg);
-void do_prog_B();
-void do_prog_C();
-void do_prog_F();
-void do_prog_H();
 
-int main (void)
+/**
+ *@author Eric Shane
+ *@brief main function
+ *@return returns nothing
+ */
+
+int main(void)
 {
-    char x;
-    x = getChoice();
-    runProgram(x);
+char x ;
+x = getChoice() ;
+runProgram(x) ;
+return 0 ;
 }
+
+
+
+
 
 bool runProgram(char whichProg)
 {
-if (whichProg != MENU_OPT_EXIT)
-    {
+
+int z=whichProg ;
     
-    if (whichProg == MENU_OPT_PROG_B)
+while (z != MENU_OPT_EXIT)
     {
-    void do_prog_B() ;
+    switch(z) 
+    {
+    case MENU_OPT_PROG_B:
+    do_prog_3B() ;
+    printf("\n Program B complete. \n") ;
+    
+    break ;
+    
+    case MENU_OPT_PROG_C:
+    do_prog_3C() ;
+    printf("\n Program C complete. \n") ;
+    
+    break ;
+    
+    case MENU_OPT_PROG_E:
+    do_prog_3E() ;
+    printf("\n Program E complete. \n") ;
+    
+    break;
+    
+    case MENU_OPT_PROG_F:
+    do_prog_3F() ;
+    printf("\n Program F complete. \n") ;
+    
+    break ;
+    
+    default: 
+    printf("ERROR \n \n") ;
+    
+    break ;
     }
     
-    else
-    
-        if (whichProg == MENU_OPT_PROG_C)
-        {
-        void do_prog_C() ;
-        }
-    
-        else
-    
-            if (whichProg == MENU_OPT_PROG_F)
-            {
-            void do_prog_F() ;
-            }
-    
-            else
-    
-                if (whichProg==MENU_OPT_PROG_H)
-                {
-                void do_prog_H() ;
-                }
-            
-                    else
-                
-                    printf("ERROR \n \n") ;
-                    getChoice() ;
-}    
+    z = getChoice() ;
+}
+
+printf(" \n \n Exit Menu \n \n") ;
+
 return (true) ;
 }
 
@@ -70,7 +161,7 @@ scanf(" %c" , &choice) ;
 return choice ;
 }
 
-void do_prog_B()
+void do_prog_3B()
 {
 
 int cash ;
@@ -78,15 +169,15 @@ int cash ;
 
 printf("First the program will ask you how much money you have.\n") ;
     
-void pause() ;
+pause() ;
 
 printf("Next you will enter how much money you have for spending.\n") ;
     
-void pause() ;
+pause() ;
 
 printf("Finally, the program will recommend where to eat based on your money.\n") ;
     
-void pause() ;
+pause() ;
 
 printf("How much cash do you have?\n") ;
 
@@ -110,7 +201,7 @@ if (cash>50)
 }
 }
 
-void do_prog_C()
+void do_prog_3C()
 {
     
 int year ;
@@ -121,21 +212,21 @@ bool oddeven ;
 
 printf("First the program will ask what year you graduated.\n") ;
     
-void pause() ;
+pause() ;
 
 printf("Next the program will have you input the year you graduated.\n") ;
     
-void pause() ;
+pause() ;
 
 printf("The program will then do math in the background to determine if you graduated in a co-ed class and if you were even or odd.\n") ;
     
-void pause() ;
+pause() ;
 
-printf("Finally, the program will greet you and state what color shirt you are recieving.\n") ;
+printf("Finally, the program will greet you and tell you what color shirt you are recieving.\n") ;
     
-void pause() ;
+pause() ;
 
-printf("What year did you graduate?") ;
+printf("What year did you graduate? \n") ;
 scanf("%d" , &year) ;
 
 {
@@ -171,12 +262,12 @@ if (coEd)
     {
     if (oddeven)
     {
-     printf("\n Hello alumni, here is your blue and green shirt. \n") ;
+     printf("\n Dear alumni, here is your blue and green shirt. \n") ;
     }
     else
         if (!oddeven)
         {
-        printf("\n Hello alumni, here is your purple and yellow shirt. \n") ;
+        printf("\n Dear alumni, here is your purple and yellow shirt. \n") ;
         }
     }
     else
@@ -185,33 +276,108 @@ if (coEd)
     {
         if (oddeven)
         {
-        printf("\n Hello alumnae, here is your curulean and aquamarine shirt. \n") ;    
+        printf("\n Dear alumnae, here is your curulean and aquamarine shirt. \n") ;    
         }
         else
             if (!oddeven)
             {
-            printf("\n Hello alumnae, here is your lavender and marigold shirt. \n") ;
+            printf("\n Dear alumnae, here is your lavender and marigold shirt. \n") ;
             }
     }
 
 }
 
-void do_prog_E()
+void do_prog_3E()
 {
+
+int temp ;
+
+printf("First you will be asked the temperature of your steak.\n") ;
     
+pause() ;
+
+printf("Next you will give the temperature of your steak to the nearest degree.\n") ;
+    
+pause() ;
+
+printf("Finally the program will tell you if your dtaek is done.\n") ;
+    
+pause() ;
+    
+printf("What is the temperature of your steak? \n") ;
+scanf("%d" , &temp) ;
+
+if (temp>145)
+{
+printf("Your steak is done. \n ") ;
+}
+else
+        if (temp==145)
+        {
+        printf("Your steak is just right. \n") ;
+        }
+        else
+            if (temp>70)
+            {
+            printf("your steak is undercooked. \n") ;
+            }
+            else
+                if (temp<70)
+                {
+                printf("Your steak is raw. \n") ;
+                }
+                else
+                    printf("Error. \n") ;
 }
 
-void do_prog_F()
+void do_prog_3F()
 {
+        printf(" First you will be asked how many times you have seen Lord of the Rings.\n") ;
     
+    pause() ;
+    
+        printf("You will input an integer value for how many times you have seen Lord of the Rings. \n") ;
+    
+    pause() ;
+    
+        printf("Based on how many times you have seen the movie, you will recieve a recommendation on how to proceed. \n") ;
+    
+    pause() ;
+    
+        printf("Finally, you will be told to bring popcorn. \n") ;
+    
+    pause() ;
+    
+int lotr ;
+    
+printf("How many times have you seen Lord of the Rings? \n \n") ;
+scanf("%d" , &lotr) ;
+    
+if (lotr>5)
+{
+printf("Maybe watch it again, for good measure. \n") ;
+}
+else
+    if (lotr>0)
+    {
+    printf("You deffinitely need to see it again. \n") ;
+    }
+    else
+        if (lotr==0)
+        {
+        printf("you need to see it! \n") ;
+        }
+        else
+            printf("error \n") ;
+    
+printf("Remember the popcorn. \n") ;
 }
 
 void pause()
 {
     
     int meaningless ;
-    printf(" Please enter a value and press Enter to continue \n ") ;
+    printf(" \n Please enter a value and press Enter to continue \n ") ;
     scanf("%d" , &meaningless) ;
     
 }
-
