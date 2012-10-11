2b3646f97a2933ed58f695fc7c5c443dea32dfee
jessica Corter <jessica corter>
2012-10-10 20:37:11 -0400

    started program 4

---
+++ cs131_program_4_jcorter.c
@@ -0,0 +1,33 @@
+/**
+ * @file cs131_Program_4_jcorter.c
+ * @author Jessica Corter
+ * @date 10/10/12
+ * @status getting started on program 4
+*/
+
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * @brief this will end the program
+*/
+const char MENU_OPT_EXIT = 2
+
+/**
+ * @brief this will run program 3B
+*/
+const char MENU_OPT_PROG_B = a
+
+/**
+ * @brief this will run program 3C
+*/
+const char MENU_OPT_PROG_C = W
+/**
+ * @brief this will run program 3D
+*/
+const char MENU_OPT_PROG_D = *
+/**
+ * @brief this will run program 3F
+*/
+const char MENU_OPT_PROG_F = ? 
+
