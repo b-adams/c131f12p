3209e453238ff2c481c967dec75eba307ab41fa9
eyonce@wells.edu <eyonce@eclipse.wells.edu>
2012-10-10 22:26:49 -0400

    Added constants and main function

--- cs131_program_4_eyonce.c
+++ cs131_program_4_eyonce.c
@@ -7,3 +7,18 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+
+const char MENU_OPT_EXIT = 2;
+const char MENU_OPT_PROG_B = a;
+const char MENU_OPT_PROG_D = W;
+const char MENU_OPT_PROG_E = *;
+const char MENU_OPT_PROG_F = ?;
+
+/**
+ *@brief Creates a menu that allows user to choose between four different questions.
+ */
+
+int main (void)
+{
+    return 0;
+}
