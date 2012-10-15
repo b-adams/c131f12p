cc1d1382972d63cb0d87c5f4a6f350ec740546fa
Melissa Fortin <mfortin@wells.edu>
2012-10-15 15:47:26 -0400

    Fixed problems

--- cs131_program_3b_mfortin.c
+++ cs131_program_3b_mfortin.c
@@ -2,12 +2,16 @@
 * @file cs131_program_3b_mfortin.c
 * @author MELISSA_FORTIN
 * @date OCTOBER_1
-* @brief Program 3: Picking four programs and implementing them 
+* @brief Program 3: Picking four programs and implementing them redo
 *
 */
 
 #include<stdio.h>
 
+/**
+@brief Entry point for program
+@returns 0 if no news (good news)
+*/
 int main (int argc, char* argv[])
 {
     int theUsersNumber;
@@ -17,13 +21,13 @@ int main (int argc, char* argv[])
     {
         printf ("Go to the Fargo\n");
     }
-    else
+    else //theUsersNumber<50
     {
         if (theUsersNumber >= 20 && theUsersNumber < 50)
         {
             printf ("Go to the Backstage Grille\n");
         }
-        else (theUsersNumber < 20);
+        else //theUsersNumber < 20
         {
             printf ("Stick to the Dining Hall\n");
         }
