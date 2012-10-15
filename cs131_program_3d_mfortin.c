4262882644aeb79611663dc93792d683a9b134bc
Melissa Fortin <mfortin@wells.edu>
2012-10-15 15:48:59 -0400

    Fixed problems

--- cs131_program_3d_mfortin.c
+++ cs131_program_3d_mfortin.c
@@ -2,33 +2,38 @@
 * @file cs131_program_3d_mfortin.c
 * @author MELISSA_FORTIN
 * @date OCTOBER_1
-* @brief Program 3: Picking four programs and implementing them 
+* @brief Program 3: Picking four programs and implementing them redo
 *
 */
 
 #include<stdio.h>
 
+/**
+@brief Entry point for program
+@returns 0 if no news (good news)
+*/
 int main (int argc, char* argv[])
 {
     int theUsersNumber;
+    int grain;
     printf ("Input how much the horse weighs\n");
     scanf ("%d", &theUsersNumber);
     if (theUsersNumber >= 955 && theUsersNumber <= 1000)
     {
-        printf ("Give the horse 2lbs of grain\n");
+        grain = 2;
     }
-    else
+    else //theUsersNumber >= 1001 && theUsersNumber <= 1100
     {
         if (theUsersNumber >= 1001 && theUsersNumber <= 1100)
         {
-            printf ("Give the horse 3lbs of grain\n");
+            grain = 3;
         }
-        else (theUsersNumber >= 1101 && theUsersNumber <= 1150);
+        else //theUsersNumber >= 1101 && theUsersNumber <= 1150
         {
-            printf ("Give the horse 4lbs of grain\n");
+            grain = 4;
         }
     }
-    printf ("The %d horse needs 3 lbs of grain and 8 lbs of hay today\n", theUsersNumber);
+    printf ("The %d horse needs %d lbs of grain and 8 lbs of hay today\n", theUsersNumber, grain);
     return 0;
 }
 
