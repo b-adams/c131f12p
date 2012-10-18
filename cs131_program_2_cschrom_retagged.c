eabcd30aadcb505e51e967709d801526846a2715
Colby Schrom <cschrom12@wells.edu>
2012-10-18 15:09:36 -0400

    Prog.2 redone. It works properly this time.

---
+++ cs131_program_2_cschrom_retagged.c
@@ -0,0 +1,145 @@
+/**
+ *@file cs131_program_2_cschrom.c
+ *@Author Colby Schrom
+ *@date 9/19/12
+ *@status Completed
+ *@assisstance Larry Force
+ *@brief The Trick, (program 2)
+ */
+
+#include <stdio.h>
+
+/**
+ @brief The answer that the trick always comes up with
+ 
+ For not-very-mysterious algebraic reasons, this trick will always come up
+ with the same answer. Rather than have '2' randomly show up in our code as
+ a 'magic number', we give it a name here.
+ */
+
+const int THE_ANSWER_EVERY_TIME = 2;
+const int timesTwo = 2;
+int twice(int n);
+int twoToThe(int exponent);
+int trickCheckArithmetic(int theUsersNumber);
+void pause();
+void trickInstructions();
+
+/**
+ *@brief Main Function
+ *@param argc
+ *@param argv
+ *@sideeffect none
+ */
+
+int main(int argc, char* argv[])
+{
+   int theUsersNumber;
+   printf ("Input a number\n");
+   scanf ("%d", &theUsersNumber);
+   trickInstructions();
+   trickCheckArithmetic(theUsersNumber);
+   printf ("The answer should be %d\n", THE_ANSWER_EVERY_TIME);
+   return 0;
+}
+
+/**
+ @brief Compute a power of 2
+ @param exponent The exponent: how many times to multiply 2 by itself.
+ @sideeffect None
+ @returns 2^n
+ 
+ This works by using the fact that 2^n is twice 2^(n-1), and 2^0 is 1
+ So, if the user gives 0 for n, this just returns n
+ Otherwise, it returns twice the value of twoToThe(n-1)
+ [We'll talk about this 'recursion' trick later in the class. It's very useful.]
+ */
+int twoToThe(int exponent)
+{
+   int theResult;
+   if(exponent==0) theResult = 1;
+   else if(exponent==1) theResult = 2;
+   else if(exponent==2) theResult = 4;
+   else theResult = 8;
+   return theResult;
+}
+
+/**
+@brief Double a number
+@param n The number to double
+@sideeffect None
+@returns Twice n
+
+Very simple function, should just return 2*n or n+n
+*/
+
+int twice(int n)
+{
+   n= n + n;
+   return n*timesTwo;
+}
+
+/**
+@brief Walk the user through the arithmetic in the trick
+@param theUsersNumber Whatever number the user picked
+@sideeffect Displays the computations for each step of the trick.
+@returns the result of going through the arithmetic of the trick
+
+Displays values of 2^k for the various k, and
+Walks through subtracting 2^0, multiplying by 2^2, adding 2^3, dividing by 2^1,
+and then subtracting twice the original number.
+*/
+
+int trickCheckArithmetic(int theUsersNumber)
+{
+   int results;
+   results = theUsersNumber;
+   printf ("You entered %d as your number.\n", theUsersNumber);
+   printf ("Now I'm going to subtract 2^0 from this number\n");
+   results = results - twoToThe(0);
+   printf ("Your number is now %d.\n", results);
+   printf ("Now im going to multiply by 2^2.\n");
+   results = results * twoToThe(2);
+   printf ("Your number is now %d.\n", results);
+   printf ("Now im going to add 2^3.\n");
+   results = results + twoToThe(3);
+   printf ("Your number is now %d.\n", results);
+   printf ("Now im going to divide by 2^1\n");
+   results = results / twoToThe(1);
+   printf ("Your number is now %d.\n", results);
+   printf ("Now im going to subtract twice your original number.\n");
+   results = results - (theUsersNumber * twoToThe(1));
+   printf ("Your number is now %d.\n", results);
+   return THE_ANSWER_EVERY_TIME;
+}
+
+
+/**
+@brief Pause until the user is ready to continue
+@sideeffect Prompts the user to press enter
+*/
+
+void pause()
+{
+   printf ("Press q then Enter\n");
+   scanf ("%d", getchar());
+   return;
+}
+
+/**
+@brief Prints \the instructions all at one time.
+@sideeffect Goes through instructions step by step at users pace
+*/
+void trickInstructions()
+{
+   printf ("First, subtract 2^0\n");
+   pause();
+   printf ("Next, multiply by 2^2\n");
+   pause();
+   printf ("Now, add 2^3\n");
+   pause();
+   printf ("And divide by 2^1\n");
+   pause();
+   printf ("Finally, subtract twice your original number.\n");
+   return;
+}
