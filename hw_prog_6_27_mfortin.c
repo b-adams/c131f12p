277f685b4764fcb7f164da3c58df651b218c2cc2
Melissa Fortin <mfortin@wells.edu>
2012-10-18 12:50:56 -0400

    I created the homework 6-27 file and added main

---
+++ hw_prog_6_27_mfortin.c
@@ -0,0 +1,43 @@
+/**
+* @file hw_prog_6_27_mfortin.c
+* @author MELISSA_FORTIN
+* @date OCTOBER_18
+* @brief Program 6-27: Homework
+*
+*/
+
+
+#include<stdio.h>
+
+/* Move one disk from source to destination through the use of recursion
+    Pre     The tower consists of n disks--source, destination, &auxillary towers given
+    Post    Steps for moves printed
+*/
+void towers (int n, char source, char dest, char auxilary);
+
+/**
+@brief Entry point for program
+@returns 0 if no news (good news)
+*/
+int main (int argc, char* argv[])
+{
+    towers (n, source, dest, auxilary);
+    return 0;
+}
+
+void towers (int n, char source, char dest, char auxilary)
+{
+    //Local destinations
+    static int step = 0;
+    //Statements
+    printf ("Towers (%d, %c, %c, %c)\n", n, source, dest, auxilary);
+    if (n == 1)
+        printf ("/t/t/t/tStep %3d: Move from %c to %c\n", ++step, source, dest);
+    else
+        {
+            towers (n - 1, source, auxilary, dest);
+            printf ("/t/t/t/tStep %3d: Move from %c to %c\n", ++step, source, dest);
+            towers (n - 1, source, auxilary, dest);
+        } // if...else
+    return;
+} //towers
