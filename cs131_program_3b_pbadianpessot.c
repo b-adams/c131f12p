41173fc6d2643c7839e7943c9c689bb7b2ec79c2
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-10-19 16:35:54 -0400

    documentation errors and magic numbers corrected

--- cs131_program_3b_pbadianpessot.c
+++ cs131_program_3b_pbadianpessot.c
@@ -1,4 +1,4 @@
-/*
+/**
  * @file cs131_program_3b_pbadianpessot.c
  * @author Pamela Badian-Pessot
  * @date October 3, 2012
@@ -7,26 +7,36 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/*
+/**
  * @brief main function
  * @param argc Number of command-line arguements
  * @param argv The comman-line tokens that were typed
  * @returns 0 if no news
  */
-
+ int main(int argc, char* argv[]);
  
+/**
+ * @brief large group are larger than 50 people
+ */
+ const int LARGE_GROUP=50;
+ 
+ /**
+  *@brief small groups are smaller than 20 people
+  */
+ const int SMALL_GROUP=20;
 
 int main(int argc, char* argv[])
 {
     int userInput;
-    
+    int largeGroup;
+    int smallGroup;
     printf("Please input how much you are having for dinner:");
     scanf("%d", &userInput);
-    if(userInput>=50)
+    if(userInput>=LARGE_GROUP)
     {
         printf("Go to the Fargo \n");
     }   else    {
-        if(userInput>=20)
+        if(userInput>=SMALL_GROUP)
         {
             printf("Go to the Backstage Grill \n");
         } else  {
