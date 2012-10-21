fdb2c3bebca2e9238e559b5f43ebf48bb3c42c12
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-10-21 16:30:17 -0400

    documentation errors corrected and redo ready for grading

--- cs131_program_4_pbadianpessot.c
+++ cs131_program_4_pbadianpessot.c
@@ -1,4 +1,4 @@
-/*
+/**
  * @file cs131_program_4_pbadianpessot.c
  * @author Pamela Badian-Pessot
  * @date October 10, 2012
@@ -7,72 +7,72 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/*
+/**
  @brief The option to exit the loop
  */
  
 const char MENU_OPT_EXIT='?';
 
-/*
+/**
  @brief The option to run program 3b
  */
  
 const char MENU_OPT_PROG_B='*';
 
-/*
+/**
  @brief The option to run program 3e
  */
  
 const char MENU_OPT_PROG_E='W';
 
-/*
+/**
  @brief The option to run program 3f
  */
  
 const char MENU_OPT_PROG_F='a';
 
-/*
+/**
  @brief The option to run program 3y
  */
  
 const char MENU_OPT_PROG_Y='2';
 
-/*
+/**
  * @brief runs program 3B, telling someone where to eat with their group
  * @returns no return, void function
  */
  
 int do_prog_3B(void);
  
-/*
+/**
  * @brief runs program 3E, telling someone that they need to watch lord of the rings
  * @returns no return, void function
  */
 
 int do_prog_3E(void);
 
-/*
+/**
  * @brief runs program 3F, telling someone how much to cook their steak
  * @returns no return, void function
  */
 
 int do_prog_3F(void);
 
-/*
+/**
  * @brief runs program 3Y, telling someone if their car is nice enough
  * @returns no return, void function
  */
  
 int do_prog_3Y(void);
 
-/*
+/**
  * @brief runs function getChoice
  * @returns no return, void function
  */
 
 char getChoice(void);
 
-/*
+/**
  * @brief runs runProgram
  * @param whichProg determines which function is run
  * @returns userChoice
@@ -80,7 +80,7 @@ char getChoice(void);
 
 bool runProgram(char whichProg);
 
-/*
+/**
  * @brief main function
  * @returns nothing
  */
