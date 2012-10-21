dfdf86d5d65b43274be6e478b01553bc81b84850
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-10-21 16:35:29 -0400

    documentation errors corrected

--- cs131_program_2_pbadianpessot.c
+++ cs131_program_2_pbadianpessot.c
@@ -10,57 +10,54 @@
 
 
 /**
- @brief The answer that the trick always comes up with
- 
- For not-very-mysterious algebraic reasons, this trick will always come up
- with the same answer. Rather than have '2' randomly show up in our code as
- a 'magic number', we give it a name here.
+ *@brief The answer that the trick always comes up with
+ * For not-very-mysterious algebraic reasons, this trick will always come up
+ * with the same answer. Rather than have '2' randomly show up in our code as
+ * a 'magic number', we give it a name here.
  */
 
 const int THE_ANSWER_EVERY_TIME=2;
 
 /**
- @brief Compute a power of 2
- @param exponent The exponent: how many times to multiply 2 by itself.
- @sideeffect None
- @returns 2^n
- 
- This works by using the fact that 2^n is twice 2^(n-1), and 2^0 is 1
- So, if the user gives 0 for n, this just returns n
- Otherwise, it returns twice the value of twoToThe(n-1)
+ *@brief Compute a power of 2
+ *@param exponent The exponent: how many times to multiply 2 by itself.
+ *@sideeffect None
+ *@returns 2^n
+ * This works by using the fact that 2^n is twice 2^(n-1), and 2^0 is 1
+ * So, if the user gives 0 for n, this just returns n
+ * Otherwise, it returns twice the value of twoToThe(n-1)
  */
 
 int toToThe(int exponent);
 
 
 /**
- @brief Walk the user through the arithmetic in the trick
- @param theUsersNumber Whatever number the user picked
- @sideeffect Displays the computations for each step of the trick.
- @returns the result of going through the arithmetic of the trick
- 
- Displays values of 2^k for the various k, and
- Walks through subtracting 2^0, multiplying by 2^2, adding 2^3, dividing by 2^1,
- and then subtracting twice the original number.
+ *@brief Walk the user through the arithmetic in the trick
+ *@param theUsersNumber Whatever number the user picked
+ *@sideeffect Displays the computations for each step of the trick.
+ *@returns the result of going through the arithmetic of the trick
+ * Displays values of 2^k for the various k, and
+ * Walks through subtracting 2^0, multiplying by 2^2, adding 2^3, dividing by 2^1,
+ * and then subtracting twice the original number.
  */
 
 int trickCheckArithmetic(int theUsersNumber);
 
 /**
- @brief Double a number
- @param n The number to double
- @sideeffect None
- @returns Twice n
- 
- Very simple function, should just return 2*n or n+n
+ *@brief Double a number
+ *@param n The number to double
+ *@sideeffect None
+ *@returns Twice n
+ *
+ *Very simple function, should just return 2*n or n+n
  */
 
 int twice(int n);
 
 /**
- @brief Lead through the arithmetic without a number
- @sideeffect print instruction and propts user through process
- @return THE_ANSWER_EVERY_TIME
+ *@brief Lead through the arithmetic without a number
+ *@sideeffect print instruction and propts user through process
+ *@return THE_ANSWER_EVERY_TIME
 */
 
 void trickInstructions();
