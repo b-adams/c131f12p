5d1670d38df375285c561084f35eecc9edf4baea
Pamela Badian-Pessot <pbadian-pessot@wells.edu>
2012-10-21 15:35:15 -0400

    documentation errors corrected

--- cs131_program_3e_pbadianpessot.c
+++ cs131_program_3e_pbadianpessot.c
@@ -1,4 +1,4 @@
-/*
+/**
  * @file cs131_program_3b_pbadianpessot.c
  * @author Pamela Badian-Pessot
  * @date October 3, 2012
@@ -7,12 +7,14 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-/*
+/**
  * @brief main function
  * @param argc Number of command-line arguements
  * @param argv The comman-line tokens that were typed
  * @returns 0 if no news
  */
+int main(int argc, char* argv[]);
+
 
  
 
