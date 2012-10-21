ed494f3aca62efa26d2187871e33a64ebfc1676c
Zakarra Butts <zmbutts12@wells.edu>
2012-10-21 16:47:37 -0400

    attempt

--- cs131_program_4_zbutts.c
+++ cs131_program_4_zbutts.c
@@ -7,8 +7,31 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-const int MENU_OPT_EXIT = *;
-const int MENU_OPT_PROG_B = W;
-const int MENU_OPT_PROG_E = a;
-const int MENU_OPT_PROG_F = ?;
+const int MENU_OPT_EXIT = *:
+const int MENU_OPT_PROG_B = W:
+const int MENU_OPT_PROG_E = a:
+const int MENU_OPT_PROG_F = ?:
 const int MENU_OPT_PROG_H = 2;
+
+int runProgram(whichProgram);
+(
+    whichProgram = 0
+)
+
+int getChoice();
+(
+    char
+)
+
+int main(void)
+{
+    
+}
+
+int runProgram(whichProgram)
+{
+    do_prog_3B();
+    do_prog_3E();
+    do_prog_3f();
+    do_prog_3h();
+}
